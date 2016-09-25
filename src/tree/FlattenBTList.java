3db5dcd8aabd57a0aba3a0a1a25e0de5c890c10a
Poorvankbhatia <puravbhatia9@gmail.com>
2016-09-25 11:36:31 +0530

    Flatten BT to list

---
+++ src/tree/FlattenBTList.java
@@ -0,0 +1,83 @@
+/*
+
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+
+ */
+
+package tree;
+
+import java.util.Stack;
+
+/**
+ * Created by poorvank on 24/09/16.
+ */
+public class FlattenBTList {
+
+    public void flatten(TreeNode root) {
+
+        if(root==null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        TreeNode prev = null;
+
+        while (!stack.isEmpty()) {
+
+            TreeNode current = stack.pop();
+
+            if(prev!=null) {
+                prev.right = current;
+                prev.left = null;
+            }
+
+            prev = current;
+
+            if(current.right!=null) {
+                stack.push(current.right);
+            }
+
+            if(current.left!=null) {
+                stack.push(current.left);
+            }
+
+
+        }
+
+        while (root!=null) {
+            System.out.println(root.right);
+            root = root.right;
+        }
+
+    }
+
+}
+
+/*
+
+f you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+
+ */
