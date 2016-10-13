e0348f55861614e16c0861e22be6fc4ee958cbe8
Poorvankbhatia <puravbhatia9@gmail.com>
2016-10-13 10:10:03 +0530

    Power function binary search

---
+++ src/binarysearch/medium/Pow.java
@@ -0,0 +1,30 @@
+/*
+
+Implement pow(x, n).
+
+ */
+package binarysearch.medium;
+
+/**
+ * Created by poorvank on 13/10/16.
+ */
+public class Pow {
+
+    public double myPow(double x, int n) {
+
+        double temp;
+        if( n == 0)
+            return 1;
+        temp = myPow(x, n/2);
+        if (n%2 == 0)
+            return temp*temp;
+        else
+        {
+            if(n > 0)
+                return x*temp*temp;
+            else
+                return (temp*temp)/x;
+        }
+    }
+
+}
