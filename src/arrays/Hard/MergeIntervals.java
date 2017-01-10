78d08dedc693ead1240eea50083df246f7fbe4d6
Poorvankbhatia <puravbhatia9@gmail.com>
2017-01-11 01:32:44 +0530

    Insert interval solution

--- src/arrays/Hard/MergeIntervals.java
+++ src/arrays/Hard/MergeIntervals.java
@@ -87,12 +87,12 @@ public class MergeIntervals {
     }
 
     public static void main(String[] args) {
-        Interval a = new Interval(2,3);
-        Interval b = new Interval(2,2);
-        Interval c = new Interval(3,3);
-        Interval d = new Interval(4,6);
-        Interval e = new Interval(1,3);
-        Interval f = new Interval(5,7);
+        Interval a = new Interval(1,2);
+        Interval b = new Interval(3,5);
+        Interval c = new Interval(6,7);
+        Interval d = new Interval(8,10);
+        Interval e = new Interval(12,16);
+        Interval f = new Interval(4,9);
         List<Interval> list = new ArrayList<Interval>(){{
             add(e);
             add(a);
@@ -109,4 +109,24 @@ public class MergeIntervals {
 
 }
 
+
+/*
+
+Same for question Insert Interval:
+
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+
+
+
+ */
 
