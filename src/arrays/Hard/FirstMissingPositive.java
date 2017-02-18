12252944bda3ace41f254459ec55948ce3526852
Poorvankbhatia <puravbhatia9@gmail.com>
2017-02-18 10:08:07 +0530

    Question Added

--- src/arrays/Hard/FirstMissingPositive.java
+++ src/arrays/Hard/FirstMissingPositive.java
@@ -1,4 +1,19 @@
+/*
+
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+
+ */
+
 package arrays.Hard;
+
+import java.util.Arrays;
+
 /**
  * Created by poorvank on 09/01/17.
  */
@@ -13,6 +28,7 @@ public class FirstMissingPositive {
             }
         }
 
+        System.out.print(Arrays.toString(arr));
 
         for (int i=0; i<n; i++) {
             if (arr[i] != i+1)    return i+1;
@@ -24,6 +40,11 @@ public class FirstMissingPositive {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,4,-1,1};
+        System.out.print(new FirstMissingPositive().firstMissingPositive(arr));
     }
 
 }
