cdc772b83de4612a125aa584d179297299f64f4a
Poorvankbhatia <puravbhatia9@gmail.com>
2017-03-26 14:50:55 +0530

    Remove boxes dp

---
+++ src/dyanamicprogramming/hard/RemoveBoxes.java
@@ -0,0 +1,85 @@
+/*
+
+Given several boxes with different colors represented by different positive numbers.
+You may experience several rounds to remove boxes until there is no box left. Each time you can choose some continuous boxes
+with the same color (composed of k boxes, k >= 1), remove them and get k*k points.
+Find the maximum points you can get.
+
+Example 1:
+Input:
+
+[1, 3, 2, 2, 2, 3, 4, 3, 1]
+Output:
+23
+Explanation:
+[1, 3, 2, 2, 2, 3, 4, 3, 1]
+----> [1, 3, 3, 4, 3, 1] (3*3=9 points)
+----> [1, 3, 3, 3, 1] (1*1=1 points)
+----> [1, 1] (3*3=9 points)
+----> [] (2*2=4 points)
+
+
+ */
+
+
+//COPIED
+package dyanamicprogramming.hard;
+
+/**
+ * Created by poorvank on 26/03/17.
+ */
+public class RemoveBoxes {
+
+    public int removeBoxes(int[] boxes) {
+        if (boxes == null || boxes.length == 0) {
+            return 0;
+        }
+
+        int size = boxes.length;
+        int[][][] dp = new int[size][size][size];
+
+        return get(dp, boxes, 0, size-1, 1);
+    }
+
+    private int get(int[][][] dp, int[] boxes, int i, int j, int k) {
+        if (i > j) {
+            return 0;
+        } else if (i == j) {
+            return k * k;
+        } else if (dp[i][j][k] != 0) {
+            return dp[i][j][k];
+        } else {
+            int temp = get(dp, boxes, i + 1, j, 1) + k * k;
+
+            for (int m = i + 1; m <= j; m++) {
+                if (boxes[i] == boxes[m]) {
+                    temp = Math.max(temp, get(dp, boxes, i + 1, m - 1, 1) + get(dp, boxes, m, j, k + 1));
+                }
+            }
+
+            dp[i][j][k] = temp;
+            return temp;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,3,2,2};
+        System.out.print(new RemoveBoxes().removeBoxes(arr));
+
+    }
+
+}
+
+/*
+
+ It turns out that the dp matrix needs one more dimension to store such state. So we are going to define the state as
+
+dp[i][j][k] represents the max points from box[i] to box[j] with k boxes whose values equal to box[i]
+The transformation function is as below
+
+dp[i][j][k] = max(dp[i+1][m-1][1] + dp[m][j][k+1]) when box[i] = box[m]
+
+ */
