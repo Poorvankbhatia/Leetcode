c0225909db2f9b983dd7598e53118b6dbf3c8269
Poorvankbhatia <puravbhatia9@gmail.com>
2017-03-27 09:50:33 +0530

    Max Points in a line refactor

--- src/math/hard/MaximumPointsInLine.java
+++ src/math/hard/MaximumPointsInLine.java
@@ -17,16 +17,6 @@ public class MaximumPointsInLine {
     class Point {
         int x;
         int y;
-
-        Point() {
-            x = 0;
-            y = 0;
-        }
-
-        Point(int a, int b) {
-            x = a;
-            y = b;
-        }
     }
 
 
