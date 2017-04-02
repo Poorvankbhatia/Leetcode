43c8974ec33617819451cfaca9d3aa8472e31736
Poorvankbhatia <puravbhatia9@gmail.com>
2017-04-02 09:06:05 +0530

    Refactor

--- src/string/medium/LongestUncommonSubsequence.java
+++ src/string/medium/LongestUncommonSubsequence.java
@@ -74,3 +74,22 @@ public class LongestUncommonSubsequence {
     }
 
 }
+
+
+/*
+
+bool isSubSequence(char str1[], char str2[], int m, int n)
+{
+    // Base Cases
+    if (m == 0) return true;
+    if (n == 0) return false;
+
+    // If last characters of two strings are matching
+    if (str1[m-1] == str2[n-1])
+        return isSubSequence(str1, str2, m-1, n-1);
+
+    // If last characters are not matching
+    return isSubSequence(str1, str2, m, n-1);
+}
+
+ */
