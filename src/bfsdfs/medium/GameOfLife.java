dd2ea0f4819c41ae5f6dd8568ac56f429cd4ec2d
Poorvankbhatia <puravbhatia9@gmail.com>
2017-04-09 17:02:58 +0530

    Source links added

--- src/bfsdfs/medium/GameOfLife.java
+++ src/bfsdfs/medium/GameOfLife.java
@@ -31,7 +31,7 @@ public class GameOfLife {
         }
 
 
-        for (int i=0;i<rows;i++) {
+        /*for (int i=0;i<rows;i++) {
             for (int j=0;j<cols;j++) {
                 int liveNeighbours = countLiveNeighbours(i,j,rows,cols,anotherBoard);
                 if(anotherBoard[i][j]==1) {
@@ -47,6 +47,27 @@ public class GameOfLife {
                         board[i][j] = 1;
                     }
                 }
+            }
+        }*/
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int lives = countLiveNeighbours(i,j,rows,cols,board);;
+
+                // In the beginning, every 2nd bit is 0;
+                // So we only need to care about when will the 2nd bit become 1.
+                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
+                    board[i][j] = 3; // Make the 2nd bit 1: 01 ---> 11
+                }
+                if (board[i][j] == 0 && lives == 3) {
+                    board[i][j] = 2; // Make the 2nd bit 1: 00 ---> 10
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] >>= 1;  // Get the 2nd state.
             }
         }
 
@@ -88,35 +109,13 @@ public class GameOfLife {
 Could you solve it in-place? Remember that the board needs to be updated at the same time:
 You cannot update some cells first and then use their updated values to update other cells.
 
-
-Since the board has ints but only the 1-bit is used, I use the 2-bit to store the new state.
-At the end, replace the old state with the new state by shifting all values one bit to the right.
-
-void gameOfLife(vector<vector<int>>& board) {
-    int m = board.size(), n = m ? board[0].size() : 0;
-    for (int i=0; i<m; ++i) {
-        for (int j=0; j<n; ++j) {
-            int count = 0;
-            for (int I=max(i-1, 0); I<min(i+2, m); ++I)
-                for (int J=max(j-1, 0); J<min(j+2, n); ++J)
-                    count += board[I][J] & 1;
-            if (count == 3 || count - board[i][j] == 3)
-                board[i][j] |= 2;
-        }
-    }
-    for (int i=0; i<m; ++i)
-        for (int j=0; j<n; ++j)
-            board[i][j] >>= 1;
-}
-Note that the above count counts the live ones among a cell's neighbors and the cell itself.
-Starting with int count = -board[i][j] counts only the live neighbors and allows the neat
-
-if ((count | board[i][j]) == 3)
+https://discuss.leetcode.com/topic/29054/easiest-java-solution-with-explanation
 
 
 In this question, we represent the board using a 2D array. In principle, the board is infinite, which would
 cause problems when the active area encroaches the border of the array. How would you address these problems?
 
+https://discuss.leetcode.com/topic/26236/infinite-board-solution
 
 
 In principle, the Life field is infinite, but computers have finite memory. This leads to problems when the active area encroaches
