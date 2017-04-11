7b7bdeb3e2d92c8a74d0e1c220128a7cacbe0669
Poorvankbhatia <puravbhatia9@gmail.com>
2017-04-11 23:32:04 +0530

    Tree Link Node

---
+++ src/tree/TreeLinkNode.java
@@ -0,0 +1,10 @@
+package tree;
+
+/**
+ * Created by poorvank on 27/09/16.
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+    public TreeLinkNode(int x) { val = x; }
+}
