38efa7e75860a1e52e07f23d92dff04260b5b59c
Poorvankbhatia <puravbhatia9@gmail.com>
2017-05-03 12:45:48 +0530

    SubArrayWithGivenSum

---
+++ src/arrays/medium/SubArraysWithGivenSum.java
@@ -0,0 +1,42 @@
+/*
+
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+
+ */
+package arrays.medium;
+
+import java.util.*;
+
+/**
+ * Created by poorvank.b on 30/04/17.
+ */
+public class SubArraysWithGivenSum {
+
+    public int subarraySum(int[] input, int k) {
+
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0,0,0,1,0,0,1,0,12,2};
+        System.out.println(new SubArraysWithGivenSum().subarraySum(arr,0));
+    }
+
+}
