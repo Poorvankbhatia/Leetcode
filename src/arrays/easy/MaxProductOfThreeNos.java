1e57863ad1f355f231d6571dbf9f0591389bf180
Poorvankbhatia <puravbhatia9@gmail.com>
2017-06-25 09:09:01 +0530

    Comments

--- src/arrays/easy/MaxProductOfThreeNos.java
+++ src/arrays/easy/MaxProductOfThreeNos.java
@@ -99,4 +99,16 @@ can be rightMax[i] or rightMin[i].
 
 Return the maximum from step 3.
 
+EASY WAY :
+
+public int maximumProduct(int[] nums) {
+
+         Arrays.sort(nums);
+         //One of the Three Numbers is the maximum value in the array.
+
+         int a = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+         int b = nums[0] * nums[1] * nums[nums.length - 1];
+         return a > b ? a : b;
+    }
+
  */
