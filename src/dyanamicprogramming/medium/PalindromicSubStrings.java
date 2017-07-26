abf04c9ae120bfebb6bffc69ed70272c9a2cd455
Poorvankbhatia <puravbhatia9@gmail.com>
2017-07-27 00:51:25 +0530

    Palindromic Substrings

---
+++ src/dyanamicprogramming/medium/PalindromicSubStrings.java
@@ -0,0 +1,79 @@
+/*
+
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+The input string length won't exceed 1000.
+
+
+ */
+package dyanamicprogramming.medium;
+
+/**
+ * Created by poorvank.b on 26/07/17.
+ */
+public class PalindromicSubStrings {
+
+    public int countSubstrings(String s) {
+
+        if(null==s || s.length()==0) {
+            return 0;
+        }
+
+        int result = 0;
+        int n = s.length();
+        boolean[][] dpTable = new boolean[n][n];
+
+        for (int i=0;i<n;i++) {
+            dpTable[i][i] =true;
+            result++;
+        }
+
+        for (int i=0;i<n-1;i++) {
+            if(s.charAt(i)==s.charAt(i+1)) {
+                dpTable[i][i+1]=true;
+                result++;
+            }
+        }
+
+        for (int gap=3;gap<=n;gap++) {
+
+            for (int i=0;i<=n-gap;i++) {
+
+                int j = i+gap-1;
+
+                if(s.charAt(i)==s.charAt(j) && dpTable[i+1][j-1]) {
+                    result++;
+                    dpTable[i][j] = true;
+                }
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(new PalindromicSubStrings().countSubstrings(s));
+    }
+
+}
+
+/*
+
+Same concept as Longest Palindromic Substring
+
+ */
