1d092ba0ea3ea8520c1f488f67984ecd43f3e997
Poorvankbhatia <puravbhatia9@gmail.com>
2017-09-22 09:16:35 +0530

    Beautiful Arrangement

---
+++ src/arrays/medium/BeautifulArrangement2.java
@@ -0,0 +1,104 @@
+/*
+Given two integers n and k, you need to construct a list which contains n different positive integers ranging from 1 to n
+and obeys the following requirement:
+Suppose this list is [a1, a2, a3, ... , an], then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
+
+If there are multiple answers, print any of them.
+
+Example 1:
+Input: n = 3, k = 1
+Output: [1, 2, 3]
+Explanation: The [1, 2, 3] has three different positive integers ranging from 1 to 3, and the [1, 1] has exactly 1 distinct integer: 1.
+Example 2:
+Input: n = 3, k = 2
+Output: [1, 3, 2]
+Explanation: The [1, 3, 2] has three different positive integers ranging from 1 to 3, and the [2, 1] has exactly 2 distinct integers: 1 and 2.
+Note:
+The n and k are in the range 1 <= k < n <= 104.
+
+ */
+package arrays.medium;
+
+import java.util.Arrays;
+
+/**
+ * Created by poorvank.b on 29/08/17.
+ */
+public class BeautifulArrangement2 {
+
+    public int[] constructArray(int n, int k) {
+
+        if(n==0) {
+            return new int[]{};
+        }
+
+        int[] result = new int[n];
+        int i=k;
+
+        result[0] = 1;int j=1;
+        boolean flag = false;
+        while (i!=0) {
+            if(!flag) {
+                result[j] = result[j-1] +  i;
+                i = -i+1;
+            } else {
+                result[j] = result[j-1] +  i;
+                i = -i-1;
+            }
+            j++;
+            flag = !flag;
+        }
+
+        if(j==n) {
+            return result;
+        }
+
+        int m = n-1;
+        while (result[m]==0) {
+            result[m] = n;
+            n--;m--;
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print(Arrays.toString(new BeautifulArrangement2().constructArray(5,4)));
+    }
+
+}
+
+/*
+
+Concise soln:
+
+if you have n number, the maximum k can be n - 1;
+if n is 9, max k is 8.
+This can be done by picking numbers interleavingly from head and tail,
+
+// start from i = 1, j = n;
+// i++, j--, i++, j--, i++, j--
+
+1   2   3   4   5
+  9   8   7   6
+out: 1 9 2 8 3 7 6 4 5
+dif:  8 7 6 5 4 3 2 1
+This is a case where k is exactly n - 1
+When k is less than that, simply lay out the rest (i, j) in incremental
+order(all diff is 1). Say if k is 5:
+
+i++, j--, i++, j--, i++, i++, i++ ...
+1   9   2   8    3 4 5 6 7
+  8   7   6   5    1 1 1 1
+
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] res = new int[n];
+        for (int i = 0, l = 1, r = n; l <= r; i++)
+            res[i] = k > 1 ? (k-- % 2 != 0 ? l++ : r--) : (k % 2 != 0? l++ : r--);
+        return res;
+    }
+}
+
+ */
