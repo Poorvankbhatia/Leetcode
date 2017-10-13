8be09c15d71d62532aadb9d75a53a659310a60b8
Poorvankbhatia <puravbhatia9@gmail.com>
2017-10-13 17:55:35 +0530

    EncodeDecode

---
+++ src/tree/medium/EncodeDecode.java
@@ -0,0 +1,67 @@
+/*
+Design an algorithm to encode a list of strings to a string.
+The encoded string is then sent over the network and is decoded back to the original list of strings.
+Machine 1 (sender) has the function:
+string encode(vector<string> strs) {
+  // ... your code
+  return encoded_string;
+}
+Machine 2 (receiver) has the function:
+vector<string> decode(string s) {
+  //... your code
+  return strs;
+}
+So Machine 1 does:
+string encoded_string = encode(strs);
+and Machine 2 does:
+vector<string> strs2 = decode(encoded_string);
+strs2 in Machine 2 should be the same as strs in Machine 1.
+Implement the encode and decode methods.
+Note:
+The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough
+to work on any possible characters.
+Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+ */
+package tree.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by poorvank.b on 08/09/17.
+ */
+public class EncodeDecode {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length()).append('/').append(s);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        while(i < s.length()) {
+            int slash = s.indexOf('/', i);
+            int size = Integer.valueOf(s.substring(i, slash));
+            ret.add(s.substring(slash + 1, slash + size + 1));
+            i = slash + size + 1;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"kjf","jkfsk","enfkm","cxz"};
+        List<String> list = Arrays.asList(arr);
+        String s  = new EncodeDecode().encode(list);
+        System.out.println(s);
+        new EncodeDecode().decode(s);
+    }
+
+}
