77e76f8409a8a77ec46a3c3034e8149549b1e0e0
Poorvankbhatia <puravbhatia9@gmail.com>
2017-11-10 20:26:26 +0530

    Remove comments

---
+++ src/string/medium/RemoveComments.java
@@ -0,0 +1,58 @@
+
+package string.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by poorvank.b on 05/11/17.
+ */
+public class RemoveComments {
+
+    public List<String> removeComments(String[] source) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        boolean mode = false;
+        for (String s : source) {
+            int i;
+            for (i = 0; i < s.length(); i++) {
+                if (mode) {
+                    if (s.charAt(i) == '*' && i < s.length() - 1 && s.charAt(i + 1) == '/') {
+                        mode = false;
+                        i++;
+                    }
+                } else {
+                    if (s.charAt(i) == '/' && i < s.length() - 1 && s.charAt(i + 1) == '/') {
+                        if (sb.length() > 0) {
+                            res.add(sb.toString());
+                        }
+                        sb = new StringBuilder();
+                        break;
+                    } else if (s.charAt(i) == '/' && i < s.length() - 1 && s.charAt(i + 1) == '*') {
+                        mode = true;
+                        i++;
+                    } else {
+                        sb.append(s.charAt(i));
+                    }
+                }
+            }
+            if (i == s.length() && !mode && sb.length() > 0) {
+                res.add(sb.toString());
+                sb = new StringBuilder();
+            }
+        }
+        return res;
+    }
+
+}
+
+/*
+
+We only need to check for two things:
+
+If we see '//' we stop reading the current line, and add whatever characters we have seen to the result
+If we see '/' then we start the multiline comment mode and we keep on ignoring characters until we see '/'
+If the current character is neither of the above two and the multiline comment mode is off, then we add that character to the current line.
+
+
+ */
