4e35b76a52164f0b7193c86c3286feacfb5b291c
Poorvankbhatia <puravbhatia9@gmail.com>
2017-11-12 10:44:32 +0530

    PivotIndex

---
+++ src/arrays/easy/PivotIndex.java
@@ -0,0 +1,65 @@
+/*
+
+Given an array of integers nums, write a method that returns the "pivot" index of this array.
+
+We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
+
+If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+
+Example 1:
+Input:
+nums = [1, 7, 3, 6, 5, 6]
+Output: 3
+Explanation:
+The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.
+Also, 3 is the first index where this occurs.
+Example 2:
+Input:
+nums = [1, 2, 3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+Note:
+
+The length of nums will be in the range [0, 10000].
+Each element nums[i] will be an integer in the range [-1000, 1000].
+
+ */
+package arrays.easy;
+
+/**
+ * Created by poorvank.b on 12/11/17.
+ */
+public class PivotIndex {
+
+    public int pivotIndex(int[] nums) {
+
+        if(nums==null || nums.length==0) {
+            return -1;
+        }
+
+        int leftSum=0;
+        int rightSum=0;
+
+        for (int element : nums) {
+            rightSum+=element;
+        }
+
+        for (int i=0;i<nums.length;i++) {
+            rightSum-=nums[i];
+            if(leftSum==rightSum) {
+                return i;
+            }
+            leftSum+=nums[i];
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1, 2, 3};
+        System.out.println(new PivotIndex().pivotIndex(arr));
+    }
+
+}
