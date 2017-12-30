2191888946fb8ebb13297f31564fcd99b6dbdc04
Poorvankbhatia <puravbhatia9@gmail.com>
2017-12-30 10:47:56 +0530

    Upside Down BT

---
+++ src/tree/medium/UpsideDownBT.java
@@ -0,0 +1,46 @@
+/*
+
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node)
+or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+For example:
+Given a binary tree {1,2,3,4,5},
+    1
+   / \
+  2   3
+ / \
+4   5
+return the root of the binary tree [4,5,2,#,#,3,1].
+   4
+  / \
+ 5   2
+    / \
+   3   1
+
+ */
+
+package tree.medium;
+
+import tree.TreeNode;
+
+/**
+ * Created by poorvank.b on 30/12/17.
+ */
+public class UpsideDownBT {
+
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+
+        if(root==null || root.left==null) {
+            return root;
+        }
+
+        TreeNode node = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return node;
+
+    }
+
+
+}
