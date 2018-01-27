faf236fd6fba24ec9b4e7fb4359799e8b2be3fa5
Poorvankbhatia <puravbhatia9@gmail.com>
2018-01-27 14:07:48 +0530

    Min Factorization and Washing machine refactored and explanation added

--- src/arrays/Hard/WashingMachines.java
+++ src/arrays/Hard/WashingMachines.java
@@ -45,35 +45,56 @@ package arrays.Hard;
 public class WashingMachines {
 
     public int findMinMoves(int[] machines) {
-        int total = 0, cnt = 0, max = 0;
-        for(int i: machines) total += i;
-        if(total % machines.length!=0) return -1;
-        int avg = total / machines.length;
-        for (int machine : machines) {
-            cnt += machine - avg;
-            max = Math.max(Math.max(max, Math.abs(cnt)), machine - avg);
+        int n = machines.length;
+        int sum = 0;
+        for (int num : machines) {
+            sum += num;
         }
-        return max;
+        if (sum % n != 0) {
+            return -1;
+        }
+        int avg = sum / n;
+        int[] leftSums = new int[n];
+        int[] rightSums = new int[n];
+        for (int i = 1; i < n; i ++) {
+            leftSums[i] = leftSums[i-1] + machines[i-1];
+        }
+        for (int i = n - 2; i >= 0; i --) {
+            rightSums[i] = rightSums[i+1] + machines[i+1];
+        }
+        int move = 0;
+        for (int i = 0; i < n; i ++) {
+            int expLeft = i * avg;
+            int expRight = (n - i - 1) * avg;
+            int left = 0;
+            int right = 0;
+            if (expLeft > leftSums[i]) {
+                left = expLeft - leftSums[i];
+            }
+            if (expRight > rightSums[i]) {
+                right = expRight - rightSums[i];
+            }
+            move = Math.max(move, left + right);
+        }
+        return move;
     }
 
 }
 
 /*
+Think about the machine i, after we make all machines have the same dresses, how many dresses will be passed through machine i?
+Let's denote the current sum of dresses of machines [0...i-1] as leftSums[i], and the current sum of dresses of machines [i+1...n-1] as rightSums[i].
+Let's denote the expected sum of dresses of machines [0...i-1] as expLeft, which means after all dresses are equally distributed, the sum of address
+in machines [0...i-1] should be expLeft. The same logic applies to machines [i+1...n-1], denoted as expRight.
 
-For example, your machines[] is [0,0,11,5]. So your total is 16
-and the target value for each machine is 4.
-Convert the machines array to a kind of gain/lose array, we get: [-4,-4,7,1].
-Now what we want to do is go from the first one and try to make all of them 0.
-To make the 1st machines 0, you need to give all its "load" to the 2nd machines.
-So we get: [0,-8,7,1]
-then: [0,0,-1,1]
-lastly: [0,0,0,0], done.
-You don't have to worry about the details about how these machines give load to each other. In this process, the least steps
-we need to eventually finish this process is determined by the peak of abs(cnt) and the max of "gain/lose" array. In this
-case, the peak of abs(cnt) is 8 and the max of gain/lose array is 7. So the result is 8.
+Then the above question should be clearly answered. If expLeft is larger than leftSums[i], that means no matter how you move the dresses, there will
+be at least expLeft - leftSums[i] dresses being moved to left of machine i, which means pass through machine i. For the right machines of machine i,
+the logic remains the same. So we could conclude that the minimum dresses passed through machine i will be:
 
-Some other example:
-machines: [0,3,0]; gain/lose array: [-1,2,-1]; max = 2, cnt = 0, -1, 1, 0, its abs peak is 1. So result is 2.
-machines: [1,0,5]; gain/lose array: [-1,-2,3]; max = 3, cnt = 0, -1, -3, 0, its abs peak is 3. So result is 3.
+left = expLeft > leftSums[i] ? expLeft - leftSums[i] : 0;
+right = expRight > rightSums[i] ? expRight - rightSums[i] : 0;
+total = left + right;
+With this answer in mind, we could know that the minimum moves is the maximum dresses that pass through for each single machine, because for each
+dress, it will require at least one move.
 
  */
--- src/math/hard/MinimumFactorization.java
+++ src/math/hard/MinimumFactorization.java
@@ -47,12 +47,12 @@ public class MinimumFactorization {
 
 /*
 
-e know that the final number generated, resres, should be such that its digits should have a product equal to the given number aa.
-In other words, the digits of resres will be the factors of the given number aa. Thus, our problem reduces to finding the factors
-(not necessarily prime) of aa and finding their smallest possible arrangement. Thus, we start with trying with the largest possible factor 99,
-obtain as many such counts of this factor as possible in resres and place such factors obtianed at its least significant positions.
+The final number generated, "res", should be such that its digits should have a product equal to the given number "a".
+In other words, the digits of "res" will be the factors of the given number "a". Thus, our problem reduces to finding the factors
+(not necessarily prime) of "a" and finding their smallest possible arrangement. Thus, we start with trying with the largest possible factor "9",
+obtain as many such counts of this factor as possible in "res" and place such factors obtained at its least significant positions.
 Then, we go on decrementing the number currently considered as the possible factor and if it is a factor, we keep on placing it at relatively
-more significant positions in resres. We go on getting such factors till we are done considering all the numbers from 9 to 2.
-At the end, resres gives the required result.
+more significant positions in "res". We go on getting such factors till we are done considering all the numbers from 9 to 2.
+At the end, "res" gives the required result.
 
  */
