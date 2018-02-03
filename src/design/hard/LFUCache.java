c29d153bb1706ec4186ad49f14832aba7d4a55e6
Poorvankbhatia <puravbhatia9@gmail.com>
2018-02-03 12:02:33 +0530

    LFU cache changed

--- src/design/hard/LFUCache.java
+++ src/design/hard/LFUCache.java
@@ -122,7 +122,7 @@ So, we need to use Collection such as TreeSet or PriorityQueue.
 Better O(1) solution :
 
 Two HashMaps are used, one to store <key, value> pair, another store the <key, node>.
-A double linked list to keep the frequeny of each key.
+A double linked list to keep the frequency of each key.
 In each double linked list node,
 keys with the same count are saved using java built in LinkedHashSet. This can keep the order.
 
@@ -149,5 +149,77 @@ it will moved into a new node with (count+1) if the node exits or it will be wra
 All your operations are guaranteed O(1) no doubt.
 
 View Discussion : https://discuss.leetcode.com/topic/69137/java-o-1-accept-solution-using-hashmap-doublelinkedlist-and-linkedhashset/2
+
+
+
+class LFUCache {
+    Node head = null;
+    final int capacity;
+    Map<Integer, Integer> valueMap = new HashMap<>();
+    Map<Integer, Node> nodeMap = new HashMap<>();
+
+    public Cache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        if (nodeMap.containsKey(key)) increase(key);
+        return valueMap.getOrDefault(key, -1);
+    }
+
+    private void increase(int key) {
+        Node node = nodeMap.get(key);
+        node.keys.remove(key);
+        if (Objects.isNull(node.next)) node.next = new Node(node, null, node.count + 1, key);
+        else if (node.next.count == node.count + 1) node.next.keys.add(key);
+        else node.next = node.next.prev = new Node(node, node.next, node.count + 1, key);
+        nodeMap.put(key, node.next);
+        if (node.keys.isEmpty()) remove(node);
+    }
+
+    private void remove(Node node) {
+        if (head == node) head = node.next;
+        else node.prev.next = node.next;
+        if (Objects.nonNull(node.next)) node.next.prev = node.prev;
+    }
+
+    public void set(int key, int value) {
+        if (0 == this.capacity) return;
+        if (Objects.nonNull(valueMap.put(key, value))) increase(key);
+        else {
+            if (nodeMap.size() == this.capacity) remove();
+            add(key);
+        }
+    }
+
+    private void add(int key) {
+        if (Objects.isNull(head)) head = new Node(null, null, 1, key);
+        else if (head.count == 1) head.keys.add(key);
+        else head = head.prev = new Node(null, head, 1, key);
+        nodeMap.put(key, head);
+    }
+
+    private void remove() {
+        int oldest = head.keys.iterator().next();
+        head.keys.remove(oldest);
+        if (head.keys.isEmpty()) remove(head);
+        nodeMap.remove(oldest);
+        valueMap.remove(oldest);
+    }
+
+    class Node {
+        public Node prev, next;
+        public final int count;
+        public LinkedHashSet<Integer> keys = new LinkedHashSet<>();
+
+        public Node(Node prev, Node next, int count, int key) {
+            this.prev = prev;
+            this.next = next;
+            this.count = count;
+            keys.add(key);
+        }
+    }
+}
+
 
  */
