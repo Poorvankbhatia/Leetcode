b7f0e72a8ffc82e62241eb180dcb10d9c22a349a
Poorvankbhatia <puravbhatia9@gmail.com>
2018-04-15 13:55:00 +0530

    Another method added for Decode ways

--- src/dyanamicprogramming/medium/DecodeWays.java
+++ src/dyanamicprogramming/medium/DecodeWays.java
@@ -52,3 +52,33 @@ public class DecodeWays {
     }
 
 }
+
+/*
+
+Another method :
+
+public int numDecodings(String s) {
+        if(s==null || s.length()==0) {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n+1];
+
+        dp[0]=1;
+        dp[1]=s.charAt(0)=='0'?0:1;
+
+        for(int i=1;i<n;i++) {
+
+            dp[i+1]=s.charAt(i)!='0'?dp[i]:0;
+            if(s.charAt(i-1)=='1' || (s.charAt(i-1)=='2' && s.charAt(i)<='6')) {
+                dp[i+1]=dp[i+1]+dp[i-1];
+            }
+
+        }
+
+        return dp[n];
+
+    }
+
+ */
