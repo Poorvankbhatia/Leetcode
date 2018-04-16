95a717f053aeb96a7f98f1a9ed1dd5300f75a892
Poorvankbhatia <puravbhatia9@gmail.com>
2018-04-16 16:41:53 +0530

    Comments added

--- src/arrays/medium/ContainerWithMostWater.java
+++ src/arrays/medium/ContainerWithMostWater.java
@@ -41,3 +41,23 @@ public class ContainerWithMostWater {
     }
 
 }
+
+/*
+
+The intuition behind this approach is that the area formed between the lines will always be limited by the height of the shorter line.
+Further, the farther the lines, the more will be the area obtained.
+
+We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines. Futher, we maintain a variable
+maxarea to store the maximum area obtained till now. At every step, we find out the area formed between them, update maxarea
+and move the pointer pointing to the shorter line towards the other end by one step.
+
+How this approach works?
+
+Initially we consider the area constituting the exterior most lines. Now, to maximize the area, we need to consider the area between the lines of larger lengths.
+If we try to move the pointer at the longer line inwards, we won't gain any increase in area, since it is limited by the shorter line.
+But moving the shorter line's pointer could turn out to be beneficial, as per the same argument, despite the reduction in the width.
+This is done since a relatively longer line obtained by moving the shorter line's pointer might overcome the reduction in area caused by the width reduction.
+
+
+
+*/
