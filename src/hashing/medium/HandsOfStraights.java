61226a525252c0ead05daecbb137d86ecf44529d
Poorvankbhatia <puravbhatia9@gmail.com>
2018-06-03 08:36:50 +0530

    changes

--- src/hashing/medium/HandsOfStraights.java
+++ src/hashing/medium/HandsOfStraights.java
@@ -1,7 +1,75 @@
+/*
+
+Alice has a hand of cards, given as an array of integers.
+
+Now she wants to rearrange the cards into groups so that each group is size W, and consists of W consecutive cards.
+
+Return true if and only if she can.
+
+
+
+Example 1:
+
+Input: hand = [1,2,3,6,2,3,4,7,8], W = 3
+Output: true
+Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8].
+Example 2:
+
+Input: hand = [1,2,3,4,5], W = 4
+Output: false
+Explanation: Alice's hand can't be rearranged into groups of 4.
+
+
+Note:
+
+1 <= hand.length <= 10000
+0 <= hand[i] <= 10^9
+1 <= W <= hand.length
+
+ */
 package hashing.medium;
+
+import java.util.TreeMap;
 
 /**
  * Created by poorvank.b on 03/06/18.
  */
 public class HandsOfStraights {
+
+    public boolean isNStraightHand(int[] hand, int W) {
+
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for (int h : hand) {
+            map.put(h,map.getOrDefault(h,0)+1);
+        }
+
+
+        while (!map.isEmpty()) {
+
+            int key = map.firstKey();
+            int count = W;
+            while (count!=0) {
+                if(map.get(key)==1) {
+                    map.remove(key);
+                } else {
+                    map.put(key,map.get(key)-1);
+                }
+                if(count>1 && !map.containsKey(key+1)) {
+                    return false;
+                }
+                key = key+1;
+                count--;
+            }
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,2000,2001,5000,5001};
+        int W = 2;
+        System.out.println(new HandsOfStraights().isNStraightHand(arr,W));
+    }
+
 }
--- src/stack/easy/BackSpaceStringCompare.java
+++ src/stack/easy/BackSpaceStringCompare.java
@@ -1,7 +1,73 @@
+/*
+
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+
+
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters.
+
+ */
 package stack.easy;
+
+import java.util.Stack;
 
 /**
  * Created by poorvank.b on 03/06/18.
  */
 public class BackSpaceStringCompare {
+
+    public boolean backspaceCompare(String S, String T) {
+
+        return getFinalString(S).equals(getFinalString(T));
+
+    }
+
+    private String getFinalString(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            if(c!='#') {
+                stack.push(c);
+            } else {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+
+    }
+
 }
