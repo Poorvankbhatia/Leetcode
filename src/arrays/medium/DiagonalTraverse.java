02429bb12c646e9fad279bd79c5f5e3d77bbbbf4
Poorvankbhatia <puravbhatia9@gmail.com>
2018-08-29 02:33:03 +0530

    Refactor

--- src/arrays/medium/DiagonalTraverse.java
+++ src/arrays/medium/DiagonalTraverse.java
@@ -19,26 +19,47 @@ package arrays.medium;
  */
 public class DiagonalTraverse {
 
-    public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) return new int[0];
-        int m = matrix.length, n = matrix[0].length;
+    class Solution {
+        public int[] findDiagonalOrder(int[][] matrix) {
+            if(matrix==null || matrix.length==0) {
+                return new int[0];
+            }
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int[] res = new int[m * n];
+            boolean up = true;
+            int r = 0, c = 0;
+            for(int i = 0; i < m * n; i++){
+                res[i] = matrix[r][c];
+                if(up){
+                    r--;
+                    c++;
+                } else {
+                    r++;
+                    c--;
+                }
 
-        int[] result = new int[m * n];
-        int row = 0, col = 0, d = 0;
-        int[][] dirs = {{-1, 1}, {1, -1}};
-
-        for (int i = 0; i < m * n; i++) {
-            result[i] = matrix[row][col];
-            row += dirs[d][0];
-            col += dirs[d][1];
-
-            if (row >= m) { row = m - 1; col += 2; d = 1 - d;}
-            if (col >= n) { col = n - 1; row += 2; d = 1 - d;}
-            if (row < 0)  { row = 0; d = 1 - d;}
-            if (col < 0)  { col = 0; d = 1 - d;}
+                if(r >= m) {
+                    r = m - 1;
+                    c += 2;
+                    up=true;
+                }
+                else if(c >= n) {
+                    c = n - 1;
+                    r += 2;
+                    up=false;
+                }
+                else if(r < 0){
+                    r = 0;
+                    up=false;
+                }
+                else if(c < 0){
+                    c = 0;
+                    up=true;
+                }
+            }
+            return res;
         }
-
-        return result;
     }
 
 }
