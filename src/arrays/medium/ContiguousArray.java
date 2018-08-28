8e9e5b3548231ac1bf5de7837f3656d293dcffee
Poorvankbhatia <puravbhatia9@gmail.com>
2018-08-28 21:39:37 +0530

    Contiguous Array refactor

--- src/arrays/medium/ContiguousArray.java
+++ src/arrays/medium/ContiguousArray.java
@@ -84,4 +84,29 @@ public class ContiguousArray {
 
 Consider all 0 values as -1. The problem now reduces to find out the maximum length subarray with sum = 0.
 
+ONE PASS:
+public int findMaxLength(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+
+        int zero=0;
+        int one=0;
+        int len=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zero++;
+            }else{
+                one++;
+            }
+
+            if(map.containsKey(zero-one)){
+                len=Math.max(len,i-map.get(zero-one));
+            }else{
+                map.put(zero-one,i);
+            }
+        }
+
+        return len;
+    }
+
  */
