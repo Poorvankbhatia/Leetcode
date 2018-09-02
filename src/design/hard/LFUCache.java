bb32cc398720d12bc59b71d9d3936bffb0b1493f
Poorvankbhatia <puravbhatia9@gmail.com>
2018-09-02 20:21:27 +0530

    LFU cache

--- src/design/hard/LFUCache.java
+++ src/design/hard/LFUCache.java
@@ -10,16 +10,145 @@ invalidate the least frequently used item before inserting a new item. For the p
  */
 package design.hard;
 
-import heap.MinPriorityQueue;
-
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 /**
  * Created by poorvank on 22/11/16.
  */
 public class LFUCache {
 
-    //Remember recentness
+    private int capacity;
+    private Node head;
+    private Map<Integer,Integer> valueMap;
+    private Map<Integer,Node> nodeMap;
+
+    private class Node {
+        private Node prev;
+        private Node next;
+        private int frequency;
+        private Queue<Integer> queue;
+
+        public Node(int frequency) {
+            this.frequency = frequency;
+            queue=new LinkedList<>();
+            this.prev=null;
+            this.next=null;
+        }
+    }
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        valueMap = new HashMap<>();
+        nodeMap = new HashMap<>();
+        head = new Node(1);
+    }
+
+    public int get(int key) {
+        if(!valueMap.containsKey(key)) {
+            return -1;
+        }
+        Node node = nodeMap.get(key);
+        changeFrequency(node,key);
+        return valueMap.get(key);
+    }
+
+    private void changeFrequency(Node node,int key) {
+        int frequency = node.frequency;
+        if(node.next!=null && node.next.frequency==frequency+1) {
+            node.next.queue.add(key);
+            nodeMap.put(key,node.next);
+        } else {
+            Node newNode = new Node(frequency+1);
+            newNode.queue.add(key);
+            nodeMap.put(key,newNode);
+            Node next = node.next;
+            node.next=newNode;
+            newNode.prev=node;
+            newNode.next=next;
+            if(next!=null) {
+                next.prev=newNode;
+            }
+        }
+        node.queue.remove(key);
+        if(node.queue.isEmpty()) {
+            removeNode(node);
+        }
+    }
+
+    private void removeNode(Node node) {
+        if(node.next!=null && node.prev!=null) {
+            node.prev.next=node.next;
+            node.next.prev=node.prev;
+        }
+        if(head==node) {
+            head=head.next;
+        }
+        if(head==null) {
+            head = new Node(1);
+        }
+    }
+
+    public void put(int key, int value) {
+        if(capacity==0) {
+            return;
+        }
+        if(!nodeMap.containsKey(key) && valueMap.size()==capacity) {
+            int toRemove = head.queue.poll();
+            nodeMap.remove(toRemove);
+            valueMap.remove(toRemove);
+            if(head.queue.isEmpty()) {
+                removeNode(head);
+            }
+        }
+        if(valueMap.containsKey(key)) {
+            changeFrequency(nodeMap.get(key),key);
+        } else {
+            if(head.frequency==1) {
+                head.queue.add(key);
+            } else {
+                Node node = new Node(1);
+                node.queue.add(key);
+                node.next=head;
+                head.prev=node;
+                head=node;
+            }
+            nodeMap.put(key,head);
+        }
+        valueMap.put(key,value);
+    }
+
+    public static void main(String[] args) {
+        LFUCache cache = new LFUCache( 2 /* capacity */ );
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        System.out.println(cache.get(2));       // returns -1 (not found)
+        System.out.println(cache.get(3));       // returns 3.
+        cache.put(4, 4);    // evicts key 1.
+        System.out.println(cache.get(1));       // returns -1 (not found)
+        System.out.println(cache.get(3));       // returns 3
+        System.out.println(cache.get(4));       // returns 4
+    }
+
+}
+
+
+/*
+
+Frequency is the time a key is accessed. Queue is a list of keys with current frequency.
+The reason to use a queue is because we want to keep track the order, i.e., we can poll out the head of the
+queue because the least recently used key will be removed in case there are multiple keys with the same frequency. To visualize look at the diagram.
+The overall complexity is O(1).
+
+
+O(logn) get and set:
+
+//Remember recentness
     private static int time=0;
 
     private class HeapNode implements Comparable<HeapNode>  {
@@ -91,135 +220,10 @@ public class LFUCache {
         }
     }
 
-
-    public static void main(String[] args) {
-        LFUCache cache = new LFUCache( 0 /* capacity */ );
-
-        cache.set(0, 0);
-        cache.set(2, 2);
-        System.out.println(cache.get(1));       // returns 1
-        cache.set(3, 3);    // evicts key 2
-        System.out.println(cache.get(2));       // returns -1 (not found)
-        System.out.println(cache.get(3));       // returns 3.
-        cache.set(4, 4);    // evicts key 1.
-        System.out.println(cache.get(1));       // returns -1 (not found)
-        System.out.println(cache.get(3));       // returns 3
-        System.out.println(cache.get(4));       // returns 4
-    }
-
-}
-
-
-/*
-
 get() and set() in average O(logn) time, or we will get TLE.
 
 hashmap to remember key-value pair.
 We need remember (frequency, recentness) for each key; and sort them to get the smallest one.
 So, we need to use Collection such as TreeSet or PriorityQueue.
-
-
-Better O(1) solution :
-
-Two HashMaps are used, one to store <key, value> pair, another store the <key, node>.
-A double linked children to keep the frequency of each key.
-In each double linked children node,
-keys with the same count are saved using java built in LinkedHashSet. This can keep the order.
-
-class Node {
-        public int count = 0;
-        public LinkedHashSet<Integer> keys = null;
-        public Node prev = null, next = null;
-
-        public Node(int count) {
-            this.count = count;
-            keys = new LinkedHashSet<Integer>();
-            prev = next = null;
-        }
-    }
-
-Every time, one key is referenced, first find the current node corresponding to the key,
-If the following node exist and the frequent is larger by one, add key to the keys of the following node,
-else create a new node and add it following the current node.
-All operations are guaranteed to be O(1).
-
-
-storing all keys with same counts in one node, if one of the keys in that node got hit once more,
-it will moved into a new node with (count+1) if the node exits or it will be wrapped into a newly created node with (count+1).
-All your operations are guaranteed O(1) no doubt.
-
-View Discussion : https://discuss.leetcode.com/topic/69137/java-o-1-accept-solution-using-hashmap-doublelinkedlist-and-linkedhashset/2
-
-
-
-class LFUCache {
-    Node head = null;
-    final int capacity;
-    Map<Integer, Integer> valueMap = new HashMap<>();
-    Map<Integer, Node> nodeMap = new HashMap<>();
-
-    public Cache(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int get(int key) {
-        if (nodeMap.containsKey(key)) increase(key);
-        return valueMap.getOrDefault(key, -1);
-    }
-
-    private void increase(int key) {
-        Node node = nodeMap.get(key);
-        node.keys.remove(key);
-        if (Objects.isNull(node.next)) node.next = new Node(node, null, node.count + 1, key);
-        else if (node.next.count == node.count + 1) node.next.keys.add(key);
-        else node.next = node.next.prev = new Node(node, node.next, node.count + 1, key);
-        nodeMap.put(key, node.next);
-        if (node.keys.isEmpty()) remove(node);
-    }
-
-    private void remove(Node node) {
-        if (head == node) head = node.next;
-        else node.prev.next = node.next;
-        if (Objects.nonNull(node.next)) node.next.prev = node.prev;
-    }
-
-    public void set(int key, int value) {
-        if (0 == this.capacity) return;
-        if (Objects.nonNull(valueMap.put(key, value))) increase(key);
-        else {
-            if (nodeMap.size() == this.capacity) remove();
-            add(key);
-        }
-    }
-
-    private void add(int key) {
-        if (Objects.isNull(head)) head = new Node(null, null, 1, key);
-        else if (head.count == 1) head.keys.add(key);
-        else head = head.prev = new Node(null, head, 1, key);
-        nodeMap.put(key, head);
-    }
-
-    private void remove() {
-        int oldest = head.keys.iterator().next();
-        head.keys.remove(oldest);
-        if (head.keys.isEmpty()) remove(head);
-        nodeMap.remove(oldest);
-        valueMap.remove(oldest);
-    }
-
-    class Node {
-        public Node prev, next;
-        public final int count;
-        public LinkedHashSet<Integer> keys = new LinkedHashSet<>();
-
-        public Node(Node prev, Node next, int count, int key) {
-            this.prev = prev;
-            this.next = next;
-            this.count = count;
-            keys.add(key);
-        }
-    }
-}
-
 
  */
