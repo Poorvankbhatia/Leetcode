cadaf47ea44a7522b90f4eadd57966b096d3f37f
Poorvankbhatia <puravbhatia9@gmail.com>
2018-09-19 08:03:33 +0530

    Dominant Index

--- src/arrays/easy/DominantIndex.java
+++ src/arrays/easy/DominantIndex.java
@@ -59,5 +59,33 @@ public class DominantIndex {
 
 /*
 
- G I
+    One Go :
+
+    public int dominantIndex(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+
+        if(nums.length == 1){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE + 1;
+        int secondMax = Integer.MIN_VALUE;
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                secondMax = max;
+                max = nums[i];
+                index = i;
+            } else if(nums[i] != max && nums[i] > secondMax){
+                secondMax = nums[i];
+            }
+        }
+        if(secondMax * 2 <= max){
+            return index;
+        }
+        return -1;
+    }
+
  */
