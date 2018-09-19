a3e5f3986166c6c2b042c7ddbda04a7311fe8fe0
Poorvankbhatia <puravbhatia9@gmail.com>
2018-09-19 09:04:05 +0530

    Reverse String 2

--- src/string/easy/ReverseString2.java
+++ src/string/easy/ReverseString2.java
@@ -69,3 +69,20 @@ public class ReverseString2 {
     }
 
 }
+
+/*
+
+public String reverseStr(String s, int k) {
+        char[] a = s.toCharArray();
+        for (int start = 0; start < a.length; start += 2 * k) {
+            int i = start, j = Math.min(start + k - 1, a.length - 1);
+            while (i < j) {
+                char tmp = a[i];
+                a[i++] = a[j];
+                a[j--] = tmp;
+            }
+        }
+        return new String(a);
+    }
+
+ */
