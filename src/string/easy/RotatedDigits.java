a9820897736df98719b6ff6e34789f46ce77f6d5
Poorvankbhatia <puravbhatia9@gmail.com>
2018-09-21 09:01:33 +0530

    Rotated Digits

---
+++ src/string/easy/RotatedDigits.java
@@ -0,0 +1,52 @@
+/*
+
+X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.
+ Each digit must be rotated - we cannot choose to leave it alone.
+
+A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other;
+6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
+
+Now given a positive number N, how many numbers X from 1 to N are good?
+
+Example:
+Input: 10
+Output: 4
+Explanation:
+There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+Note:
+
+N  will be in range [1, 10000].
+
+ */
+package string.easy;
+
+/**
+ * Created by poorvank.b on 21/09/18.
+ */
+public class RotatedDigits {
+
+    public int rotatedDigits(int N) {
+        int count = 0;
+        for (int i = 1; i <= N; i ++) {
+            if (isValid(i)) count ++;
+        }
+        return count;
+    }
+
+    private boolean isValid(int N) {
+        boolean validFound = false;
+        while (N > 0) {
+            if (N % 10 == 2) validFound = true;
+            if (N % 10 == 5) validFound = true;
+            if (N % 10 == 6) validFound = true;
+            if (N % 10 == 9) validFound = true;
+            if (N % 10 == 3) return false;
+            if (N % 10 == 4) return false;
+            if (N % 10 == 7) return false;
+            N = N / 10;
+        }
+        return validFound;
+    }
+
+}
