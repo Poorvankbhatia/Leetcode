cda640c304e390230ee75bddc038cd07dda49b7c
Poorvankbhatia <puravbhatia9@gmail.com>
2018-10-21 17:30:28 +0530

    MakingLarge island refactor

--- src/bfsdfs/hard/MakingLargeIsland.java
+++ src/bfsdfs/hard/MakingLargeIsland.java
@@ -30,6 +30,8 @@ Notes:
  */
 package bfsdfs.hard;
 
+import bfsdfs.UnionFind;
+
 import java.util.HashMap;
 
 /**
@@ -38,23 +40,23 @@ import java.util.HashMap;
 public class MakingLargeIsland {
 
 
-    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int largestIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        UF uf = new UF(m, n);
+        UnionFind uf = new UnionFind(m*n);
         int res = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    uf.sizes[i * n + j] = 1;
                     for (int[] dir : dirs) {
                         res = Math.max(res, connect(i, j, i + dir[0], j + dir[1], grid, uf, m, n));
                     }
                 }
             }
         }
+
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -66,68 +68,34 @@ public class MakingLargeIsland {
         return res;
     }
 
-    private int connect(int a, int b, int i, int j, int[][] grid, UF uf, int m, int n) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) return 0;
+    private int connect(int a, int b, int i, int j, int[][] grid, UnionFind uf, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) {
+            return 0;
+        }
         return uf.union(a * n + b, i * n + j);
     }
 
-    public int check(int i, int j, UF uf, int m, int n) {
+    public int check(int i, int j, UnionFind uf, int m, int n) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int[] dir : dirs) {
             int x = dir[0] + i;
             int y = dir[1] + j;
-            if (x < 0 || y < 0 || x >= m || y >= n) continue;
+            if (x < 0 || y < 0 || x >= m || y >= n) {
+                continue;
+            }
             int r = uf.find(x * n + y);
-            map.put(r, uf.sizes[r]);
+            map.put(r, uf.size[r]);
         }
         int res = 1;
-        for (int k : map.keySet()) res += map.get(k);
+        for (int k : map.keySet()){
+            res += map.get(k);
+        }
         return res;
     }
 
-    class UF {
-        int[] roots;
-        int[] sizes;
-        int m, n;
-
-        private UF(int m, int n) {
-            roots = new int[m * n];
-            for (int i = 0; i < m * n; i++) roots[i] = i;
-            sizes = new int[m * n];
-            this.m = m;
-            this.n = n;
-        }
-
-        private int find(int i) {
-
-            if (i != roots[i]) roots[i] = find(roots[i]);
-            return roots[i];
-        }
-
-        private int union(int a, int b) {
-            int ra = find(a);
-            int rb = find(b);
-            if (sizes[ra] == 0 || sizes[rb] == 0) return 0;
-            if (ra == rb) return sizes[ra];
-            else {
-                if (sizes[ra] > sizes[rb]) {
-                    sizes[ra] += sizes[rb];
-                    roots[rb] = ra;
-                    return sizes[ra];
-                } else {
-                    sizes[rb] += sizes[ra];
-                    roots[ra] = rb;
-                    return sizes[rb];
-                }
-            }
-        }
-    }
-
-
     public static void main(String[] args) {
         int[][] grid = new int[][]{
-                {1,1},
-                {1,0}
+                {1}
         };
         System.out.println(new MakingLargeIsland().largestIsland(grid));
     }
