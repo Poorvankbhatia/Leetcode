cf18a4cfcfb4b6aaa035c81dd9cfbba9691cdc89
Poorvankbhatia <puravbhatia9@gmail.com>
2018-10-21 11:11:52 +0530

    Refactor

--- src/bfsdfs/hard/BusRoutes.java
+++ src/bfsdfs/hard/BusRoutes.java
@@ -109,4 +109,16 @@ public class BusRoutes {
 
 The idea is BFS the bus, not the stop. If we get the a stop which a bus pass by, then we can get to every stop of this route.
 
+For each of the bus stop, we maintain all the buses (bus routes) that go through it. To do that, we use a HashMap,
+where bus stop number is the key and all the buses (bus routes) that go through it are added to an ArrayList.
+
+We use BFS, where we process elements in a level-wise manner. We add the Start bus stop in the queue. Next, when we enter the while loop,
+we add all the bus stops that are reachable by all the bus routes that go via the Start.
+Thus, if we have the input as [[1, 2, 7], [3, 6, 7]] and Start as 6, then upon processing bus stop 6, we would add bus stops 3 and 7.
+With this approach, all the bus stops at a given level, are "equal distance" from the start node, in terms of number of buses that need to be changed.
+
+To avoid loops, we also maintain a HashSet that stores the buses that we have already visited.
+
+
+
  */
