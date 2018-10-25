225acced58e7ca9bf710a441e324c02125019007
Poorvankbhatia <puravbhatia9@gmail.com>
2018-10-25 07:33:52 +0530

    changes

--- src/math/easy/XKindOFdeck.java
+++
@@ -1,7 +0,0 @@
-package math.easy;
-
-/**
- * Created by poorvank.b on 08/10/18.
- */
-public class XKindOFdeck {
-}
--- src/tree/easy/IncreasingOrderSEarchTree.java
+++
@@ -1,7 +0,0 @@
-package tree.easy;
-
-/**
- * Created by poorvank.b on 02/09/18.
- */
-public class IncreasingOrderSEarchTree {
-}
