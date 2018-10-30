0700e245298b94da6eff0649c60f8eb0d57377f8
Poorvankbhatia <puravbhatia9@gmail.com>
2018-10-30 16:22:55 +0530

    brick falling when hit : complexity added

--- src/bfsdfs/hard/BrickFallingWhenHit.java
+++ src/bfsdfs/hard/BrickFallingWhenHit.java
@@ -150,6 +150,6 @@ the island size is just enlarged by 1, which means no brick drops.
 
 Time Complexity: O(N∗Q∗α(N∗Q)), where N = R*C is the number of grid squares, Q is the length of hits, and α is the Inverse-Ackerman function.
 
-Space Complexity: O(N)O(N).
+Space Complexity: O(N).
 
  */
