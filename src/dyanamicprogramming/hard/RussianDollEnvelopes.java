7280b538f2b3917eb62ff3c3108a0edd991bc262
Poorvankbhatia <puravbhatia9@gmail.com>
2018-11-05 09:48:38 +0530

    Russian doll envelopes : Refactor

--- src/dyanamicprogramming/hard/RussianDollEnvelopes.java
+++ src/dyanamicprogramming/hard/RussianDollEnvelopes.java
@@ -10,73 +10,88 @@ What is the maximum number of envelopes can you Russian doll? (put one inside ot
 package dyanamicprogramming.hard;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 /**
  * Created by poorvank on 10/12/16.
  */
 public class RussianDollEnvelopes {
 
-    private class Envelope implements Comparable<Envelope> {
-        int width;
-        int height;
-
-        public Envelope(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
-
-        public int compareTo(Envelope e) {
-            if(this.height>e.height) {
-                return 1;
-            }  else if(this.height<e.height) {
-                return -1;
-            } else{
-                return 0;
-            }
-        }
-    }
-
     public int maxEnvelopes(int[][] envelopes) {
-
-        if(envelopes.length==0 || envelopes.length==1) {
-            return envelopes.length;
+        if(envelopes==null || envelopes.length==0) {
+            return 0;
         }
+
+        Arrays.sort(envelopes,(a,b)->(a[0]!=b[0])?a[0]-b[0]:b[1]-a[1]);
 
         int n = envelopes.length;
-        Envelope[] envelopeArr = new Envelope[n];
 
-        for (int i=0;i<n;i++) {
-            envelopeArr[i] = new Envelope(envelopes[i][0],envelopes[i][1]);
-        }
-        Arrays.sort(envelopeArr);
+        int[][] dp = new int[n][2];
 
-        int[] count  = new int[n];
-        for (int i=0;i<n;i++) {
-            count[i] = 1;
-        }
+        dp[0] = envelopes[0];
+        int len=0;
 
-        int max=1;
-
-        for (int i=1;i<n;i++) {
-            for (int j=0;j<i;j++) {
-                if(envelopeArr[i].height>envelopeArr[j].height && envelopeArr[i].width>envelopeArr[j].width && count[i]<count[j] +1) {
-                    count[i] = count[j] +1;
-                    if(max<count[i]) {
-                        max = count[i];
-                    }
-                }
+        for(int i=1;i<n;i++) {
+            int pos = getpos(dp,len,envelopes[i]);
+            System.out.println(pos+" "+"("+envelopes[i][0]+","+envelopes[i][1]+")");
+            if(dp[pos][1]>envelopes[i][1]) {
+                dp[pos][0] = envelopes[i][0];
+                dp[pos][1] = envelopes[i][1];
+            }
+            if(pos>len) {
+                len=pos;
+                dp[len][0]=envelopes[i][0];
+                dp[len][1]=envelopes[i][1];
             }
         }
 
-        return max;
+        return len+1;
 
+    }
+
+    private int getpos(int[][] dp,int len,int[] envelope) {
+
+        int start=0;
+        int end = len;
+
+        System.out.println("len "  + len);
+
+        if(dp[start][1]>envelope[1]) {
+            return start;
+        }
+
+        if(dp[end][1]<envelope[1]) {
+            return end+1;
+        }
+
+        while(start<end) {
+            int mid =start+(end-start)/2;
+            if(dp[mid][1]==envelope[1]) {
+                return mid;
+            }
+            if(dp[mid][1]>envelope[1]) {
+                end=mid;
+            } else {
+                start=mid+1;
+            }
+        }
+
+        return start;
+
+    }
+
+    public static void main(String[] args) {
+        int[][] a = new int[][]{
+                {1,1},{1,1},{1,1}
+        };
+        System.out.println(new RussianDollEnvelopes().maxEnvelopes(a));
     }
 
 }
 
 
 /*
+
+[[1,2],[2,3],[3,4],[3,5],[4,5],[5,5],[5,6],[6,7],[7,8]]
 
 A variation of LIS
 
