f8ea1bfef43918e14aef84cd77b6e45c07e365d0
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-02 10:25:15 +0530

    Flip Equivalent

---
+++ src/tree/medium/FlipEquivalent.java
@@ -0,0 +1,79 @@
+
+/*
+
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Write a function that determines whether two binary trees are flip equivalent.  The trees are given by root nodes root1 and root2.
+
+
+
+Example 1:
+
+Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+Output: true
+Explanation: We flipped at nodes with values 1, 3, and 5.
+
+ */
+package tree.medium;
+
+import tree.TreeNode;
+
+/**
+ * Created by poorvank.b on 02/12/18.
+ */
+public class FlipEquivalent {
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == root2)
+            return true;
+        if (root1 == null || root2 == null || root1.val != root2.val)
+            return false;
+
+        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
+                flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+    }
+
+}
+
+
+/*
+
+Time Complexity: O(min(N_1, N_2)) are the lengths of root1 and root2.
+
+Method 2:
+
+Flip each node so that the left child is smaller than the right, and call this the canonical representation.
+All equivalent trees have exactly one canonical representation.
+
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        List<Integer> vals1 = new ArrayList();
+        List<Integer> vals2 = new ArrayList();
+        dfs(root1, vals1);
+        dfs(root2, vals2);
+        return vals1.equals(vals2);
+    }
+
+    public void dfs(TreeNode node, List<Integer> vals) {
+        if (node != null) {
+            vals.add(node.val);
+            int L = node.left != null ? node.left.val : -1;
+            int R = node.right != null ? node.right.val : -1;
+
+            if (L < R) {
+                dfs(node.left, vals);
+                dfs(node.right, vals);
+            } else {
+                dfs(node.right, vals);
+                dfs(node.left, vals);
+            }
+        }
+    }
+}
+Time Complexity: O(N_1 + N_2) where N_1, N_2N  are the lengths of root1 and root2
+​
+ )
+
+ */
