71ea1c3cda3de6830a4e8761dbfb6f103b145294
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-02 10:15:48 +0530

    changes

--- src/greedy/easy/Stock2.java
+++ src/greedy/easy/Stock2.java
@@ -9,56 +9,31 @@ the same time (ie, you must sell the stock before you buy again).
  */
 package greedy.easy;
 
-import java.util.ArrayList;
-
 /**
  * Created by poorvank.b on 07/11/17.
  */
 public class Stock2 {
 
-    private static class Interval {
-
-        public int sell;
-        public int buy;
-
-    }
-
     public int maxProfit(int[] prices) {
 
-        if(prices==null || prices.length==0) {
-            return 0;
-        }
-
-        ArrayList<Interval> arrayList = new ArrayList<>();
-
-        int i = 0;
-
-        while (i < prices.length) {
-
-            Interval interval = new Interval();
-
-            while (i < prices.length - 1 && (prices[i] >= prices[i + 1])) {
+        if(prices==null||prices.length==0) return 0;
+        int i=0;
+        int peak=prices[0];
+        int valley=prices[0];
+        int maxProfit=0;
+        int len=prices.length;
+        while (i<len-1){
+            while (i<len-1 && prices[i]>=prices[i+1]){//down
                 i++;
             }
-
-            interval.buy = i++;
-
-            while (i < prices.length && (prices[i] >= prices[i - 1])) {
+            valley=prices[i];
+            while (i<len-1 && prices[i]<=prices[i+1]){//up
                 i++;
             }
-
-            interval.sell = i - 1;
-
-            arrayList.add(interval);
-
+            peak = prices[i];
+            maxProfit+=peak-valley;
         }
-
-        int max=0;
-        for (Interval interval : arrayList) {
-            max += prices[interval.sell]-prices[interval.buy];
-        }
-
-        return max;
+        return maxProfit;
 
 
     }
--- src/heap/hard/MinimumCostToHire.java
+++ src/heap/hard/MinimumCostToHire.java
@@ -42,16 +42,23 @@ public class MinimumCostToHire {
 
     public double mincostToHireWorkers(int[] q, int[] w, int K) {
         double[][] workers = new double[q.length][2];
-        for (int i = 0; i < q.length; ++i)
-            workers[i] = new double[]{(double)(w[i]) / q[i], (double)q[i]};
+
+        for (int i = 0; i < q.length; ++i) {
+            workers[i] = new double[]{(double) (w[i]) / q[i], (double) q[i]};
+        }
         Arrays.sort(workers, (a, b) -> Double.compare(a[0], b[0]));
         double res = Double.MAX_VALUE, qsum = 0;
         PriorityQueue<Double> pq = new PriorityQueue<>();
-        for (double[] worker: workers) {
+
+        for (double[] worker : workers) {
             qsum += worker[1];
             pq.add(-worker[1]);
-            if (pq.size() > K) qsum += pq.poll();
-            if (pq.size() == K) res = Math.min(res, qsum * worker[0]);
+            if (pq.size() > K) {
+                qsum += pq.poll();
+            }
+            if (pq.size() == K) {
+                res = Math.min(res, qsum * worker[0]);
+            }
         }
         return res;
     }
