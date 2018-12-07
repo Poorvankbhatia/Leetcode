4c7ca524562dbdcf3c125ffc1a9488d9b664a12d
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-07 05:57:48 +0530

    Refactor

--- src/bfsdfs/hard/StrobogrammaticNumber3.java
+++ src/bfsdfs/hard/StrobogrammaticNumber3.java
@@ -17,6 +17,7 @@ package bfsdfs.hard;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -39,15 +40,14 @@ public class StrobogrammaticNumber3 {
     }
 
     private List<String> helper(int cur, int max){
-        if(cur == 0) return new ArrayList<String>(Arrays.asList(""));
-        if(cur == 1) return new ArrayList<String>(Arrays.asList("1", "8", "0"));
+        if(cur == 0) return new ArrayList<>(Collections.singletonList(""));
+        if(cur == 1) return new ArrayList<>(Arrays.asList("1", "8", "0"));
 
         List<String> rst = new ArrayList<String>();
         List<String> center = helper(cur - 2, max);
 
-        for(int i = 0; i < center.size(); i++){
-            String tmp = center.get(i);
-            if(cur != max) rst.add("0" + tmp + "0");
+        for (String tmp : center) {
+            if (cur != max) rst.add("0" + tmp + "0");
             rst.add("1" + tmp + "1");
             rst.add("6" + tmp + "9");
             rst.add("8" + tmp + "8");
