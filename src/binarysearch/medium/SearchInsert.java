b5acdacc965d1d8ca033616ea7c105d382d32df8
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-15 14:49:56 +0530

    Refactor

--- src/binarysearch/medium/SearchInsert.java
+++ src/binarysearch/medium/SearchInsert.java
@@ -62,3 +62,28 @@ public class SearchInsert {
     }
 
 }
+
+/*
+
+Another soln:
+
+public int searchInsert(int[] nums, int target) {
+
+        int low=0,high=nums.length-1;
+
+        while(low<=high) {
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target) {
+                return mid;
+            } else if(nums[mid]<target) {
+                low=mid+1;
+            } else {
+                high=mid;
+            }
+        }
+
+        return low;
+
+    }
+
+ */
