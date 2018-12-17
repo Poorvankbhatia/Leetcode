7d11ee916e95481c14ce80d5d778a3b4c998d32a
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-17 23:26:11 +0530

    Min Depth refactor

--- src/tree/easy/MinDepth.java
+++ src/tree/easy/MinDepth.java
@@ -55,3 +55,20 @@ public class MinDepth {
     }
 
 }
+
+/*
+
+Another Sol:
+
+public int minDepth(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left,right) + 1;
+
+    }
+
+ */
