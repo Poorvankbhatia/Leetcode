a0e0813875b55c2d2f8ccd3ca1a9784f5ff203a2
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-17 23:35:53 +0530

    PathSum

--- src/tree/easy/PathSum.java
+++ src/tree/easy/PathSum.java
@@ -47,3 +47,23 @@ public class PathSum {
     }
 
 }
+
+/*
+
+Alternate Sol:
+
+public boolean hasPathSum(TreeNode root, int sum) {
+
+        if(root==null) {
+            return false;
+        }
+
+        if(root.left==null && root.right==null) {
+            return sum==root.val;
+        }
+
+        return hasPathSum(root.left,sum-root.val) || hasPathSum(root.right,sum-root.val);
+
+    }
+
+ */
