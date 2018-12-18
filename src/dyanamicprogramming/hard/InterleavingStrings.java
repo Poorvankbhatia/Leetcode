d6ac5ba844ce273bae554e3fae0ea0a5a5019ae7
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-18 09:12:17 +0530

    Interleaving strings refactor

--- src/dyanamicprogramming/hard/InterleavingStrings.java
+++ src/dyanamicprogramming/hard/InterleavingStrings.java
@@ -54,7 +54,7 @@ public class InterleavingStrings {
     }
 
     public static void main(String[] args) {
-        System.out.println(new InterleavingStrings().isInterleave("aa","ab","abaa"));
+        System.out.println(new InterleavingStrings().isInterleave("a","","a"));
     }
 
 }
@@ -91,5 +91,50 @@ bool isInterleaved(char *A, char *B, char *C)
 
 Time Complexity: O(MN)
 Auxiliary Space: O(MN)
+
+BFS Sol:
+
+Imagine a grid, which x-axis and y-axis are s1 and s2, matching s3 is the same as
+finding a path from (0,0) to (len1, len2). It actually becomes a
+BFS on grid. Since we don't need exact paths, a HashSet of
+coordinates is used to eliminate duplicated paths.
+
+
+
+ public boolean isInterleave(String s1, String s2, String s3) {
+        int len1 = s1.length(),
+            len2 = s2.length(),
+            len3 = s3.length();
+        if (len1 + len2 != len3) return false;
+        Deque<Integer> queue = new LinkedList<>();
+        int matched = 0;
+        queue.offer(0);
+        Set<Integer> set = new HashSet<>();
+        while (queue.size() > 0 && matched < len3) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int p1 = queue.peek() / len3,
+                    p2 = queue.peek() % len3;
+                queue.poll();
+                if (p1 < len1 && s1.charAt(p1) == s3.charAt(matched)) {
+                    int key = (p1 + 1) * len3 + p2;
+                    if (!set.contains(key)) {
+                        set.add(key);
+                        queue.offer(key);
+                    }
+                }
+                if (p2 < len2 && s2.charAt(p2) == s3.charAt(matched)) {
+                    int key = p1 * len3 + (p2 + 1);
+                    if (!set.contains(key)) {
+                        set.add(key);
+                        queue.offer(key);
+                    }
+                }
+            }
+            matched++;
+        }
+        return queue.size() > 0 && matched == len3;
+    }
+
 
  */
