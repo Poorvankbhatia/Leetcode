17f3521c42da6169cae432557f35e380330a871a
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-18 19:33:40 +0530

    Inorder traversal

--- src/tree/medium/InorderTraversal.java
+++ src/tree/medium/InorderTraversal.java
@@ -52,4 +52,27 @@ public class InorderTraversal {
 
 }
 
+/*
 
+Better method:
+
+public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<Integer>();
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode cur = root;
+
+    while(cur!=null || !stack.empty()){
+        while(cur!=null){
+            stack.add(cur);
+            cur = cur.left;
+        }
+        cur = stack.pop();
+        list.add(cur.val);
+        cur = cur.right;
+    }
+
+    return list;
+}
+
+ */
