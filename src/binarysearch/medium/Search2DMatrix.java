f2eab1f89cffbd209f2220424a46a2840c51683b
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-22 19:22:08 +0530

    Search 2D Refactor

--- src/binarysearch/medium/Search2DMatrix.java
+++ src/binarysearch/medium/Search2DMatrix.java
@@ -49,3 +49,44 @@ public class Search2DMatrix {
 
 
 }
+
+/*
+
+Another solution:
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+
+        int m = matrix.length;
+        if(m==0) {
+            return false;
+        }
+        int n = matrix[0].length;
+        if(n==0) {
+            return false;
+        }
+        int l=0,r=(m*n)-1;
+
+        while(l<r) {
+
+            int mid = l+(r-l)/2;
+
+            if(matrix[mid/n][mid%n]==target) {
+                return true;
+            }
+            if(matrix[mid/n][mid%n]>target) {
+                r=mid;
+            } else {
+                l=mid+1;
+            }
+
+        }
+
+        return matrix[l/n][l%n]==target;
+
+
+    }
+}
+
+ */
