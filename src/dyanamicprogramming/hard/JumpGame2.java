409e13fd1647bcaf1eb1eb141b407ff958a8e31e
Poorvankbhatia <puravbhatia9@gmail.com>
2018-12-26 11:40:21 +0530

    Jump game 2 refactor

--- src/dyanamicprogramming/hard/JumpGame2.java
+++ src/dyanamicprogramming/hard/JumpGame2.java
@@ -21,19 +21,30 @@ public class JumpGame2 {
 
     public int jump(int[] nums) {
 
-        int currentEnd=0,currentFarthest=0,jumps=0;
-
-        for (int i=0;i<nums.length-1;i++) {
-
-            currentFarthest = Math.max(currentFarthest,i+nums[i]);
-            if(i==currentEnd) {
-                jumps++;
-                currentEnd=currentFarthest;
+        if(nums.length==1) {
+            return 0;
+        }
+        int start=0,end=0;
+        int jump=0;
+        int i=0;
+        while(i<nums.length) {
+            if(start>end) {
+                break;
             }
-
+            int far=0;
+            for(int j=start;j<=end;j++) {
+                far = Math.max(far,nums[j]+j);
+                if(far>=nums.length-1) {
+                    return jump+1;
+                }
+            }
+            start=end+1;
+            end=far;
+            jump++;
+            i++;
         }
 
-        return jumps;
+        return -1;
 
     }
 
@@ -45,9 +56,14 @@ public class JumpGame2 {
 
 }
 
-/**
- * The main idea is based on greedy. Let's say the range of the current jump is [curBegin, curEnd],
- * curFarthest is the farthest point that all points in [curBegin, curEnd] can reach.
- * Once the current point reaches curEnd, then trigger another jump, and set the new curEnd with curFarthest, then keep the above steps.
- *
+/*
+
+Try to change this problem to a BFS problem, where nodes in level i are all the nodes that can be reached in i-1th jump.
+for example. 2 3 1 1 4 , is
+2||
+3 1||
+1 4 ||
+
+clearly, the minimum jump of 4 is 2 since 4 is in level 3
+
  */
