73ce80acefaa23532b37708f6425cf9ea42fb497
Poorvankbhatia <puravbhatia9@gmail.com>
2019-01-01 12:43:53 +0530

    Path Sum 3 & Delete node BST refactor

--- src/tree/easy/PathSum3.java
+++ src/tree/easy/PathSum3.java
@@ -83,3 +83,35 @@ public class PathSum3 {
 }
 
 //Calculate count of paths for every node.
+
+/*
+
+Another sol:
+
+public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        return findPathSum(root, 0, sum, map);
+    }
+    private int findPathSum(TreeNode curr, int sum, int target, Map<Integer, Integer> map) {
+        if (curr == null) {
+            return 0;
+        }
+        // update the prefix sum by adding the current val
+        sum += curr.val;
+        // get the number of valid path, ended by the current node
+        int numPathToCurr = map.getOrDefault(sum-target, 0);
+        // update the map with the current sum, so the map is good to be passed to the next recursion
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        // add the 3 parts discussed in 8. together
+        int res = numPathToCurr + findPathSum(curr.left, sum, target, map)
+                                               + findPathSum(curr.right, sum, target, map);
+       // restore the map, as the recursion goes from the bottom to the top
+        map.put(sum, map.get(sum) - 1);
+        return res;
+    }
+
+ */
