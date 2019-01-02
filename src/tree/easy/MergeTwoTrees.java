dd10e5a6972e2304da1225200aab3c403af3ffff
Poorvankbhatia <puravbhatia9@gmail.com>
2019-01-02 10:56:17 +0530

    Refactor: Merge Trees

--- src/tree/easy/MergeTwoTrees.java
+++ src/tree/easy/MergeTwoTrees.java
@@ -35,29 +35,19 @@ public class MergeTwoTrees {
 
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
 
-        return merge(t1,t2);
-
-    }
-
-
-    private TreeNode merge(TreeNode t1,TreeNode t2) {
-
+        if(t1==null && t2==null) {
+            return null;
+        }
         if(t1==null) {
             return t2;
         }
         if(t2==null) {
             return t1;
         }
-
-        TreeNode mergeRoot = new TreeNode(t1.val+t2.val);
-
-        TreeNode left = merge(t1.left,t2.left);
-        TreeNode right = merge(t1.right,t2.right);
-
-        mergeRoot.left = left;
-        mergeRoot.right = right;
-
-        return mergeRoot;
+        TreeNode root = new TreeNode(t1.val+t2.val);
+        root.left=mergeTrees(t1.left,t2.left);
+        root.right=mergeTrees(t1.right,t2.right);
+        return root;
 
 
     }
