3263adec33fdc43c92219e6ed4c145fc83cfb67e
Poorvankbhatia <puravbhatia9@gmail.com>
2019-01-10 10:28:02 +0530

    Shortest path to get all keys

--- src/bfsdfs/hard/ShortestPathToGetAllKeys.java
+++ src/bfsdfs/hard/ShortestPathToGetAllKeys.java
@@ -1,3 +1,37 @@
+/*
+
+We are given a 2-dimensional grid. "." is an empty cell, "#" is a wall, "@" is the starting point, ("a", "b", ...) are keys,
+and ("A", "B", ...) are locks.
+
+We start at the starting point, and one move consists of walking one space in one of the 4 cardinal directions.  We cannot walk
+outside the grid, or walk into a wall.  If we walk over a key, we pick it up.  We can't walk over a lock unless we have the corresponding key.
+
+For some 1 <= K <= 6, there is exactly one lowercase and one uppercase letter of the first K letters of the English alphabet in the grid.
+This means that there is exactly one key for each lock, and one lock for each key; and also that the letters used to represent the keys
+and locks were chosen in the same order as the English alphabet.
+
+Return the lowest number of moves to acquire all keys.  If it's impossible, return -1.
+
+
+
+Example 1:
+
+Input: ["@.a.#","###.#","b.A.B"]
+Output: 8
+Example 2:
+
+Input: ["@..aA","..B#.","....b"]
+Output: 6
+
+
+Note:
+
+1 <= grid.length <= 30
+1 <= grid[0].length <= 30
+grid[i][j] contains only '.', '#', '@', 'a'-'f' and 'A'-'F'
+The number of keys is in [1, 6].  Each key has a different letter and opens exactly one lock.
+
+ */
 package bfsdfs.hard;
 
 import java.util.HashSet;
@@ -10,81 +44,74 @@ import java.util.Set;
  */
 public class ShortestPathToGetAllKeys {
 
-    int[][] dir = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+    /*
 
-    public int shortestPathAllKeys(String[] grid) {
+    Use Bit to represent the keys.
+    Use State to represent visited states.
 
-        if(grid==null || grid.length==0) {
-            return -1;
+     */
+    class State {
+        int keys, i, j;
+        State(int keys, int i, int j) {
+            this.keys = keys;
+            this.i = i;
+            this.j = j;
         }
-        int m = grid.length;
-        int n = grid[0].length();
-
-        Queue<int[]> queue = new LinkedList<>();
-
-        int keyCount=0;
-
-        for (int i=0;i<m;i++) {
-            for (int j=0;j<n;j++) {
-                if(grid[i].charAt(j)=='@') {
-                    queue.add(new int[]{i,j});
+    }
+    public int shortestPathAllKeys(String[] grid) {
+        int x = -1, y = -1, m = grid.length, n = grid[0].length(), max = -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char c = grid[i].charAt(j);
+                if (c == '@') {
+                    x = i;
+                    y = j;
                 }
-                if(grid[i].charAt(j)>='a' && grid[i].charAt(j)<='z') {
-                    keyCount++;
+                if (c >= 'a' && c <= 'f') {
+                    max = Math.max(c - 'a' + 1, max);
                 }
             }
         }
-
-
-        Set<Character> keySet = new HashSet<>();
-        boolean[][] visited = new boolean[grid.length][grid[0].length()];
-
-        int jumps=0;
-        visited[queue.peek()[0]][queue.peek()[1]]=true;
-        while (!queue.isEmpty()) {
-            int size=queue.size();
-            while (size>0) {
-                if(keyCount==0) {
-                    return jumps;
+        State start = new State(0, x, y);
+        Queue<State> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        visited.add(0 + " " + x + " " + y);
+        q.offer(start);
+        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                State cur = q.poll();
+                if (cur.keys == (1 << max) - 1) {
+                    return step;
                 }
-                int[] pop = queue.poll();
-                int i = pop[0];
-                int j = pop[1];
-                for (int[] d : dir) {
-                    int nextI = d[0]+i;
-                    int nextJ = d[1]+j;
-
-                    if(isSafe(nextI,nextJ,m,n,visited,grid)) {
-                        if(grid[nextI].charAt(nextJ)>='a' && grid[nextI].charAt(nextJ)<='z')  {
-                            keySet.add(grid[nextI].charAt(nextJ));
-                            keyCount--;
-                            if(keyCount==0) {
-                                return jumps+1;
-                            }
-                            queue.add(new int[]{nextI,nextJ});
-                            visited[nextI][nextJ]=true;
-                        } else if(grid[nextI].charAt(nextJ)>='A' && grid[nextI].charAt(nextJ)<='Z') {
-                            if(keySet.contains(Character.toLowerCase(grid[nextI].charAt(nextJ)))) {
-                                queue.add(new int[]{nextI,nextJ});
-                                visited[nextI][nextJ]=true;
-                            }
-                        } else  {
-                            queue.add(new int[]{nextI,nextJ});
-                            visited[nextI][nextJ]=true;
+                for (int[] dir : dirs) {
+                    int i = cur.i + dir[0];
+                    int j = cur.j + dir[1];
+                    int keys = cur.keys;
+                    if (i >= 0 && i < m && j >= 0 && j < n) {
+                        char c = grid[i].charAt(j);
+                        if (c == '#') {
+                            continue;
+                        }
+                        //if we have to find keys abc and now we have found ab, state is 110(2) = 6; If we have found ac, state is 101(2) = 5.
+                        if (c >= 'a' && c <= 'f') {
+                            keys = keys | 1 << (c - 'a'); // Same concept as Sum of all paths BT
+                        }
+                        if (c >= 'A' && c <= 'F' && ((keys >> (c - 'A')) & 1) == 0) {
+                            continue;
+                        }
+                        if (!visited.contains(keys + " " + i + " " + j)) {
+                            visited.add(keys + " " + i + " " + j);
+                            q.offer(new State(keys, i, j));
                         }
                     }
                 }
-                size--;
             }
-            jumps++;
+            step++;
         }
-
         return -1;
-
-    }
-
-    private boolean isSafe(int i,int j,int m,int n,boolean[][] visited,String[] grid) {
-        return i>=0 && j>=0 && i<m && j<n && !visited[i][j] && grid[i].charAt(j)!='#';
     }
 
     public static void main(String[] args) {
@@ -93,3 +120,9 @@ public class ShortestPathToGetAllKeys {
     }
 
 }
+
+/*
+
+Complexity : which is 2^k * m * n
+
+ */
