e9e7e684353635f5ca65c66ca2951b89a2f18604
Poorvankbhatia <puravbhatia9@gmail.com>
2019-01-12 12:37:25 +0530

    Coin change 2

--- src/dyanamicprogramming/medium/CoinChange2.java
+++ src/dyanamicprogramming/medium/CoinChange2.java
@@ -75,4 +75,17 @@ public class CoinChange2 {
 
 Variation of knapsack
 
+Using O(m) space:
+
+ public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+
  */
