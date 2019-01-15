77ebd41bf408254846c17a0709b573196df51f38
Poorvankbhatia <puravbhatia9@gmail.com>
2019-01-15 10:15:48 +0530

    Remove Duplicates

--- src/greedy/hard/RemoveDuplicates.java
+++ src/greedy/hard/RemoveDuplicates.java
@@ -106,6 +106,62 @@ after one letter is determined in step 3, it need to be removed from the "last a
 should be ignored in the following steps
 in step 3, the beginning index of the search range should be the index of previous determined letter plus one
 
-https://discuss.leetcode.com/topic/43469/java-o-n-solution-using-stack-with-detail-explanation
+
+O(n) method stack:
+
+public String removeDuplicateLetters(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int[] count = new int[256];
+        for(int i=0;i<n;i++) {
+            count[s.charAt(i)-'a']++;
+        }
+
+        boolean[] visited = new boolean[256];
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            count[c-'a']--;
+            if(visited[c-'a']) {
+                continue;
+            }
+            while(!stack.isEmpty() && stack.peek()>c && count[stack.peek()-'a']>0) {
+                visited[stack.pop()-'a']=false;
+            }
+            stack.push(c);
+            visited[c-'a']=true;
+
+        }
+
+        for(char c : stack) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+
+    }
+    
+   First, given "bcabc", the solution should be "abc". If we think about this problem intuitively, you would sort of go
+   from the beginning of the string and start removing one if there is still the same character left and a smaller character
+   is after it. Given "bcabc", when you see a 'b', keep it and continue with the search, then keep the following 'c', then we see
+   an 'a'. Now we get a chance to get a smaller lexi order, you can check if after 'a', there is still 'b' and 'c' or not.
+   We indeed have them and "abc" will be our result.
+
+Come to the implementation, we need some data structure to store the previous characters 'b' and 'c', and we need to compare
+the current character with previous saved ones, and if there are multiple same characters, we prefer left ones. This calls for a stack.
+
+After we decided to use stack, the implementation becomes clearer. From the intuition, we know that we need to know if there are
+still remaining characters left or not. So we need to iterate the array and save how many each characters are there. A visited array
+is also required since we want unique character in the solution. The line while(!stack.isEmpty() && stack.peek() > c && count[stack.peek()-'a'] > 0)
+checks that the queued character should be removed or not, like the 'b' and 'c' in the previous example.
+After removing the previous characters, push in the new char and mark the visited array.
+
+Time complexity: O(n), n is the number of chars in string.
+
+Space complexity: O(n) worst case.
+
+
+
 
  */
