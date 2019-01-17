cf9597cde59fe6b5dc9d29e40c4d76b1cf118525
Poorvankbhatia <puravbhatia9@gmail.com>
2019-01-17 18:09:59 +0530

    Refactor

--- src/hashing/hard/OddEvenJump.java
+++ src/hashing/hard/OddEvenJump.java
@@ -76,15 +76,23 @@ public class OddEvenJump {
 
     public int oddEvenJumps(int[] A) {
         int n  = A.length, res = 1;
-        boolean[] higher = new boolean[n], lower = new boolean[n];
+        boolean[] higher = new boolean[n];
+        boolean[] lower = new boolean[n];
         higher[n - 1] = lower[n - 1] = true;
         TreeMap<Integer, Integer> map = new TreeMap<>();
         map.put(A[n - 1], n - 1);
         for (int i = n - 2; i >= 0; --i) {
-            Integer hi = map.ceilingKey(A[i]), lo = map.floorKey(A[i]);
-            if (hi != null) higher[i] = lower[map.get(hi)];
-            if (lo != null) lower[i] = higher[map.get(lo)];
-            if (higher[i]) res++;
+            Integer hi = map.ceilingKey(A[i]);
+            Integer lo = map.floorKey(A[i]);
+            if (hi != null) {
+                higher[i] = lower[map.get(hi)];
+            }
+            if (lo != null) {
+                lower[i] = higher[map.get(lo)];
+            }
+            if (higher[i]) {
+                res++;
+            }
             map.put(A[i], i);
         }
         return res;
