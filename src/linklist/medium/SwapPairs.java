36a4d531c88493ad26246a60feda924357cbcacf
poorvank <poorvank@amazon.com>
2019-04-28 13:19:30 +0530

    Swap pairs

---
+++ src/linklist/medium/SwapPairs.java
@@ -0,0 +1,27 @@
+/*
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+ */
+package linklist.medium;
+
+import linklist.ListNode;
+
+public class SwapPairs {
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
