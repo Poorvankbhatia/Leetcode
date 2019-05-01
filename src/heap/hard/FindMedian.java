9b6a579a2604eeb81c453e8c5bac0529c774b1f5
poorvank <poorvank@amazon.com>
2019-05-01 21:50:55 +0530

    Find Median

--- src/heap/hard/FindMedian.java
+++ src/heap/hard/FindMedian.java
@@ -24,49 +24,52 @@ findMedian() -> 2
 
 package heap.hard;
 
-import heap.MaxPriorityQueue;
-import heap.MinPriorityQueue;
+import java.util.PriorityQueue;
 
 /**
  * Created by poorvank on 06/09/16.
  */
 public class FindMedian {
 
-    private MinPriorityQueue<Integer> minPriorityQueue = new MinPriorityQueue<>();
-    private MaxPriorityQueue<Integer> maxPriorityQueue = new MaxPriorityQueue<>();
-    private double median = 0;
+    private PriorityQueue<Integer> max;
+    private PriorityQueue<Integer> min;
+    private double median;
 
-    // Adds a number into the data structure.
+    /** initialize your data structure here. */
+    public FindMedian() {
+        min = new PriorityQueue<>();
+        max = new PriorityQueue<>((x, y)->y-x);
+        median = 0.0;
+    }
+
     public void addNum(int num) {
-        int sizeDiff = maxPriorityQueue.getSize()-minPriorityQueue.getSize();
-        if(sizeDiff==1) {
-            if(num>median) {
-                minPriorityQueue.insert(num);
+        if(median<num) {
+            if(max.size()==min.size()) {
+                min.add(num);
+                median = min.peek();
+            } else if(max.size()>min.size()) {
+                min.add(num);
+                median = (double) (max.peek()+min.peek())/2;
             } else {
-                minPriorityQueue.insert(maxPriorityQueue.deleteMax());
-                maxPriorityQueue.insert(num);
+                max.add(min.poll());
+                min.add(num);
+                median = (double) (max.peek()+min.peek())/2;
             }
-            median = (double) (maxPriorityQueue.getMaximumElement()+minPriorityQueue.getMinimumElement())/2;
-        } else if(sizeDiff==0) {
-            if(num>median) {
-                minPriorityQueue.insert(num);
-                median = minPriorityQueue.getMinimumElement();
+        } else {
+            if(max.size()==min.size()) {
+                max.add(num);
+                median = max.peek();
+            } else if(max.size()>min.size()) {
+                min.add(max.poll());
+                max.add(num);
+                median = (double) (max.peek()+min.peek())/2;
             } else {
-                maxPriorityQueue.insert(num);
-                median = maxPriorityQueue.getMaximumElement();
+                max.add(num);
+                median = (double) (max.peek()+min.peek())/2;
             }
-        } else if(sizeDiff==-1) {
-            if(num>median) {
-                maxPriorityQueue.insert(minPriorityQueue.deleteMin());
-                minPriorityQueue.insert(num);
-            } else {
-                maxPriorityQueue.insert(num);
-            }
-            median = (double) (maxPriorityQueue.getMaximumElement()+minPriorityQueue.getMinimumElement())/2;
         }
     }
 
-    // Returns the median of current data stream
     public double findMedian() {
         return median;
     }
