32c56f6a7542afd6a2053bdbede79038b109456b
poorvank <poorvank@amazon.com>
2019-05-05 18:50:46 +0530

    Alien Dictionary sorted.

---
+++ src/string/easy/AlienDictionary.java
@@ -0,0 +1,76 @@
+/*
+
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet
+ is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are
+ sorted lexicographicaly in this alien language.
+
+
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical
+ rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character.
+
+
+Note:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are english lowercase letters.
+
+ */
+package string.easy;
+
+public class AlienDictionary {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] count = new int[26];
+        int val=0;
+        for(char c : order.toCharArray()) {
+            count[c-'a']=val++;
+        }
+        int n = words.length;
+        for(int i=n-1;i>0;i--) {
+            if(compare(words[i],words[i-1],count)>0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    private int compare(String s1,String s2,int[] count) {
+        int i;
+        for(i=0;i<Math.min(s1.length(),s2.length());i++) {
+            if(count[s1.charAt(i)-'a']<count[s2.charAt(i)-'a']) {
+                return 1;
+            } else if(count[s1.charAt(i)-'a']>count[s2.charAt(i)-'a']) {
+                return -1;
+            }
+        }
+
+        if(i==s1.length() && i==s2.length()) {
+            return 0;
+        } else if(i==s1.length()) {
+            return 1;
+        }else {
+            return -1;
+        }
+
+    }
+}
