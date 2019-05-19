4c8d89cd5f65d9769673bc2f9e334bedc64245da
poorvank <poorvank@amazon.com>
2019-05-19 09:46:42 +0530

    Stack soln added

--- src/string/easy/RemoveDuplicates.java
+++ src/string/easy/RemoveDuplicates.java
@@ -48,3 +48,28 @@ public class RemoveDuplicates {
         return chars;
     }
 }
+
+
+/*
+
+Stack soln:
+
+ public String removeDuplicates(String S) {
+        Stack<Character> stack = new Stack();
+
+        for (char ch : S.toCharArray()) {
+            if (stack.isEmpty() || ch != stack.peek()) {
+                stack.push(ch);
+            } else {
+                stack.pop();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+ */
