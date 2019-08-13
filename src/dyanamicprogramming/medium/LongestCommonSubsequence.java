25ba7a46d7998dcbe01ee8db69f0654f215a84b9
poorvank <puravbhatia9@gmail.com>
2019-08-14 04:45:44 +0530

    Longest common subsequence

---
+++ src/dyanamicprogramming/medium/LongestCommonSubsequence.java
@@ -0,0 +1,61 @@
+/*
+
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A subsequence of a string is a new string generated from the original string with some characters(can be none)
+deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde"
+while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+
+
+
+If there is no common subsequence, return 0.
+
+
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace"
+Output: 3
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+
+
+Constraints:
+
+1 <= text1.length <= 1000
+1 <= text2.length <= 1000
+The input strings consist of lowercase English characters only.
+
+ */
+package dyanamicprogramming.medium;
+
+public class LongestCommonSubsequence {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<=m;i++) {
+            for(int j=0;j<=n;j++) {
+                if(i==0 || j==0) {
+                    dp[i][j]=0;
+                } else if(text1.charAt(i-1)==text2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+}
