d114c1810640d647011362b948e62cc978af42b9
poorvank <puravbhatia9@gmail.com>
2019-08-27 15:46:43 +0530

    Largest lexicographic substring

---
+++ src/string/hard/LargestSubstringLexicographic.java
@@ -0,0 +1,43 @@
+/*
+
+Given a string s, return the last substring of s in lexicographical order.
+
+
+
+Example 1:
+
+Input: "abab"
+Output: "bab"
+Explanation: The substrings are ["a", "ab", "aba", "abab", "b", "ba", "bab"]. The lexicographically maximum substring is "bab".
+Example 2:
+
+Input: "leetcode"
+Output: "tcode"
+
+
+Note:
+
+1 <= s.length <= 4 * 10^5
+s contains only lowercase English letters.
+
+ */
+package string.hard;
+
+public class LargestSubstringLexicographic {
+    public String lastSubstring(String s) {
+        int i = 0, j = 1, offset = 0, len = s.length();
+        while (i + offset < len && j + offset < len) {
+            char c = s.charAt(i + offset), d = s.charAt(j + offset);
+            if (c == d) {
+                ++offset;
+            }else {
+                if (c < d)  { i += offset + 1; } // chars in [i, ..., i + offset] <= charAt(i) == charAt(j)
+                else { j += offset + 1; } // c > d, chars in [j, ..., j + offset] <= charAt(i) == charAt(j)
+                if (i == j) { ++i; } // avoid duplicate start indices.
+                offset = 0; // reset offset to 0.
+            }
+        }
+        return s.substring(Math.min(i, j));
+    }
+
+}
