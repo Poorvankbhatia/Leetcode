314dd1bfb893915e17498c1a379e6cc634460501
poorvank <puravbhatia9@gmail.com>
2019-09-05 09:44:30 +0530

    Codec.

---
+++ src/string/medium/Codec.java
@@ -0,0 +1,108 @@
+/*
+
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+Machine 1 (sender) has the function:
+
+string encode(vector<string> strs) {
+  // ... your code
+  return encoded_string;
+}
+Machine 2 (receiver) has the function:
+vector<string> decode(string s) {
+  //... your code
+  return strs;
+}
+So Machine 1 does:
+
+string encoded_string = encode(strs);
+and Machine 2 does:
+
+vector<string> strs2 = decode(encoded_string);
+strs2 in Machine 2 should be the same as strs in Machine 1.
+
+Implement the encode and decode methods.
+
+
+
+Note:
+
+The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
+Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+
+
+ */
+package string.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length());
+            sb.append("(").append(s).append(")");
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> result = new ArrayList<>();
+        int i=0;
+        while(i<s.length()) {
+            StringBuilder sb = new StringBuilder();
+            int j=i;
+            while(s.charAt(j)!='(') {
+                sb.append(s.charAt(j++));
+            }
+            int x = Integer.parseInt(sb.toString());
+            result.add(s.substring(j+1,j+x+1));
+            i=j+x+2;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("helloworld");
+        list.add("world");
+        String encode = new Codec().encode(list);
+        System.out.println(encode);
+        new Codec().decode(encode);
+    }
+
+}
+
+
+/*
+Better version:
+public class Codec {
+	// Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length()).append('/').append(s);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        while(i < s.length()) {
+            int slash = s.indexOf('/', i);
+            int size = Integer.valueOf(s.substring(i, slash));
+            i = slash + size + 1;
+            ret.add(s.substring(slash + 1, i));
+        }
+        return ret;
+    }
+}
+ */
