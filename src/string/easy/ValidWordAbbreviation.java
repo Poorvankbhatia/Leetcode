247cac9c92de4b7f0fab9392e5987f636ca9d739
poorvank <puravbhatia9@gmail.com>
2019-09-21 12:18:29 +0530

    Valid Word Abbreviation.

--- src/string/easy/ValidWordAbbreviation.java
+++ src/string/easy/ValidWordAbbreviation.java
@@ -26,60 +26,30 @@ package string.easy;
 public class ValidWordAbbreviation {
 
     public boolean validWordAbbreviation(String word, String abbr) {
-        if (word.length() == 0) {
-            return abbr.length() == 0;
-        }
-        int w = 0, ab = 0;
-        while (w < word.length() && ab < abbr.length()) {
-            char a = abbr.charAt(ab);
-            if (Character.isLetter(a)) {
-                if (word.charAt(w) != abbr.charAt(ab)) {
-                    return false;
-                }
-                w++;
-                ab++;
-            } else if (Character.isDigit(a)) {
-                if (a == '0') {
-                    return false;
-                }
-                int org = ab;
-                while (ab < abbr.length() && Character.isDigit(abbr.charAt(ab))) {
-                    ab++;
-                }
-                int num = Integer.valueOf(abbr.substring(org, ab));
-                while (w < word.length() && num > 0) {
-                    w++;
-                    num--;
-                }
-                if (num > 0) {
-                    return false;
-                }
-            } else {
+        int i = 0, j = 0;
+        while (i < word.length() && j < abbr.length()) {
+            if (word.charAt(i) == abbr.charAt(j)) {
+                ++i;++j;
+                continue;
+            }
+            if (abbr.charAt(j) <= '0' || abbr.charAt(j) > '9') {
                 return false;
             }
+            int start = j;
+            while (j < abbr.length() && abbr.charAt(j) >= '0' && abbr.charAt(j) <= '9') {
+                ++j;
+            }
+            int num = Integer.valueOf(abbr.substring(start, j));
+            i += num;
         }
-        return true;
+        return i == word.length() && j == abbr.length();
     }
 
 }
 
 /*
 
-Given a non-empty string s and an abbreviation abbr, return whether the string matches with the given abbreviation.
-A string such as "word" contains only the following valid abbreviations:
-["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
-Notice that only the above abbreviations are valid abbreviations of the string "word". Any other string is not a valid abbreviation of "word".
-Note:
-Assume s contains only lowercase letters and abbr contains only lowercase letters and digits.
-Example 1:
-Given s = "internationalization", abbr = "i12iz4n":
-
-Return true.
-
-Example 2:
-Given s = "apple", abbr = "a2e":
-
-Return false.
+FACEBOOK
 
 
  */
