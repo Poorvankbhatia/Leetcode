577b6e2f3a43dc9883cfb461191a6aa05d8ab1b8
poorvank <puravbhatia9@gmail.com>
2019-09-27 10:07:57 +0530

    Critical Connections.

---
+++ src/graph/hard/CriticalConnections.java
@@ -0,0 +1,88 @@
+/*
+
+There are n servers numbered from 0 to n-1 connected by undirected server-to-server connections forming a network where
+ connections[i] = [a, b] represents a connection between servers a and b. Any server can reach any other server directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some server unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+Output: [[1,3]]
+Explanation: [[3,1]] is also accepted.
+
+
+Constraints:
+
+1 <= n <= 10^5
+n-1 <= connections.length <= 10^5
+connections[i][0] != connections[i][1]
+There are no repeated connections.
+
+ */
+package graph.hard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CriticalConnections {
+
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        int[] disc = new int[n], low = new int[n];
+        // use adjacency list instead of matrix will save some memory, adjmatrix will cause MLE
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.fill(disc, -1); // use disc to track if visited (disc[i] == -1)
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        // build graph
+        for (List<Integer> connection : connections) {
+            int from = connection.get(0), to = connection.get(1);
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (disc[i] == -1) {
+                dfs(i, low, disc, graph, res, 0);
+            }
+        }
+        return res;
+    }
+
+    int time = 0; // time when discover each vertex
+
+    private void dfs(int u, int[] low, int[] disc, List<Integer>[] graph, List<List<Integer>> res, int pre) {
+        disc[u] = low[u] = ++time; // discover u
+        for (int j = 0; j < graph[u].size(); j++) {
+            int v = graph[u].get(j);
+            if (v == pre) {
+                continue; // if parent vertex, ignore
+            }
+            if (disc[v] == -1) { // if not discovered
+                dfs(v, low, disc, graph, res, u);
+                low[u] = Math.min(low[u], low[v]);
+                if (low[v] > disc[u]) {
+                    // u - v is critical, there is no path for v to reach back to u or previous vertices of u
+                    res.add(Arrays.asList(u, v));
+                }
+            } else { // if v discovered and is not parent of u, update low[u], cannot use low[v] because u is not subtree of v
+                low[u] = Math.min(low[u], disc[v]);
+            }
+        }
+    }
+
+}
+
+/*
+
+Basically, it uses dfs to travel through the graph to find if current vertex u, can travel back to u or previous vertex
+low[u] records the lowest vertex u can reach
+disc[u] records the time when u was discovered
+
+
+https://www.geeksforgeeks.org/bridge-in-a-graph/
+
+ */
