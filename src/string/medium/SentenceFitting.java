ef6e0862f5ea1c5da23ee2478cc14c7172562b64
poorvank <puravbhatia9@gmail.com>
2019-10-02 14:04:11 +0530

    Refactor Sentence fitting.

--- src/string/medium/SentenceFitting.java
+++ src/string/medium/SentenceFitting.java
@@ -146,4 +146,39 @@ Given there are n characters in sentence[]
 
 Time complexity: O(rows + n)
 
+
+Another sol:
+
+sub-problem: if there's a new line which is starting with certain index in sentence, what is the starting index of next line (nextIndex[]).
+BTW, we compute how many times the pointer in current line passes over the last index (times[]).
+relation : ans += times[i], i = nextIndex[i], for _ in 0..<row. where i indicates what is the first word in the current line.
+Time complexity : O(n*(cols/lenAverage)) + O(rows), where n is the length of sentence array, lenAverage is the average length of the words in the input array.
+
+public int wordsTyping(String[] sentence, int rows, int cols) {
+        int[] nextIndex = new int[sentence.length];
+        int[] times = new int[sentence.length];
+        for(int i=0;i<sentence.length;i++) {
+            int curLen = 0;
+            int cur = i;
+            int time = 0;
+            while(curLen + sentence[cur].length() <= cols) {
+                curLen += sentence[cur++].length()+1;
+                if(cur==sentence.length) {
+                    cur = 0;
+                    time ++;
+                }
+            }
+            nextIndex[i] = cur;
+            times[i] = time;
+        }
+        int ans = 0;
+        int cur = 0;
+        for(int i=0; i<rows; i++) {
+            ans += times[cur];
+            cur = nextIndex[cur];
+        }
+        return ans;
+    }
+
+
  */
