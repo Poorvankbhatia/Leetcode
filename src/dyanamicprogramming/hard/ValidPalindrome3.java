fc610adaf3bb82da074894998afdbe6bcc35359c
poorvank <puravbhatia9@gmail.com>
2019-10-05 21:45:14 +0530

    Valid Palindrome 3

---
+++ src/dyanamicprogramming/hard/ValidPalindrome3.java
@@ -0,0 +1,49 @@
+package dyanamicprogramming.hard;
+
+public class ValidPalindrome3 {
+
+    public boolean isValidPalindrome(String str, int k) {
+        int n = str.length();
+
+        StringBuilder stringBuilder = new StringBuilder(str).reverse();
+        int lps = lcs(str, stringBuilder.toString(), n, n);
+
+        return (n - lps <= k);
+    }
+
+    /*
+    longest palindromic subsequence:
+    LCS of the given string & its reverse will be the longest palindromic sequence.
+     */
+    private int lcs(String X, String Y, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+}
+
+/*
+
+The idea is to find the longest palindromic subsequence(lps) of the given string.
+|lps - original string| <= k,
+then the string is k-palindrome.
+
+Eg:
+
+One of the lps of string pqrstrp is prsrp.
+Characters not contributing to lps of the
+string should be removed in order to make the string palindrome. So on removing q and s (or t) from pqrstrp,
+string will transform into a palindrome.
+
+ */
