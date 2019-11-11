6c10e87bfe155f30e275ccc51fbd8af6b8d12956
poorvank <puravbhatia9@gmail.com>
2019-11-11 22:01:37 +0530

    Number of dice rolls.

--- src/dyanamicprogramming/medium/NumberOfDiceRolls.java
+++ src/dyanamicprogramming/medium/NumberOfDiceRolls.java
@@ -52,33 +52,53 @@ import java.util.Map;
 
 public class NumberOfDiceRolls {
 
-    int MOD = 1000000000 + 7;
-    Map<String, Integer> memo = new HashMap<>();
+    int mod = (int) (Math.pow(10,9)+7);
+    Map<String,Integer> map = new HashMap<>();
     public int numRollsToTarget(int d, int f, int target) {
-        if (d == 0 && target == 0) {
-            return 1;
+        if(d==1) {
+            if(f>=target) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
-        if (d == 0 || target == 0) {
-            return 0;
+        if(map.containsKey(d+"_"+target)) {
+            return map.get(d+"_"+target);
         }
-        String str = d + " " + target;
-        if (memo.containsKey(str)) {
-            return memo.get(str);
-        }
-        int res = 0;
-        for (int i = 1; i <= f; i++) {
-            if (target >= i) {
-                res = (res + (numRollsToTarget(d - 1, f, target - i) % MOD)) % MOD;
+        int way = 0;
+        for(int i=1;i<=f;i++) {
+            if(target>i) {
+                way= (way+numRollsToTarget(d-1,f,target-i))%mod;
             } else {
                 break;
             }
         }
-        memo.put(str, res);
-        return res;
+
+        map.put(d+"_"+target,(way));
+        return way;
     }
 
 }
 
 /*
 O(d*f*target)
+
+Bottom up version:
+public int numRollsToTarget(int d, int f, int target) {
+        int MOD = (int)Math.pow(10, 9) + 7;
+        long[][] dp = new long[d + 1][target + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= d; i++) {
+            for (int j = 0; j <= target; j++) {
+                for (int k = 1; k <= f; k++) {
+                    if (j >= k) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return (int)dp[d][target];
+    }
  */
