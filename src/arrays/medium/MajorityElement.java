9dc3888ae1fa75060da8cb36a5912e27fa4b565e
poorvank <puravbhatia9@gmail.com>
2019-11-14 23:06:25 +0530

    Majority Element: Indentation correction.

--- src/arrays/medium/MajorityElement.java
+++ src/arrays/medium/MajorityElement.java
@@ -16,37 +16,30 @@ import java.util.List;
 public class MajorityElement {
 
     public List<Integer> majorityElement(int[] nums) {
-
-        int k =3;
-        return majorityElementUtil(nums,k);
-
+        int k = 3;
+        return majorityElementUtil(nums, k);
     }
 
-    private List<Integer> majorityElementUtil(int[] nums,int k) {
-
+    private List<Integer> majorityElementUtil(int[] nums, int k) {
         List<Integer> list = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        if (nums.length == 0) return list;
-
-
-        if(nums.length<k) {
-            for (Integer n  : nums) {
-                map.put(n,1);
-            }
-            for (Integer e  : map.keySet()) {
-                list.add(e);
-            }
-
+        if (nums.length == 0) {
             return list;
         }
-
+        if (nums.length < k) {
+            for (Integer n : nums) {
+                map.put(n, 1);
+            }
+            list.addAll(map.keySet());
+            return list;
+        }
         for (int n : nums) {
             if (map.containsKey(n)) map.put(n, map.get(n) + 1);
-            //there are at most k-1 elements that could have more than n/k occurrences in an array,
-            // for example, at most 2 distinct number could have a frequency of 1/3 in an array.
-            else if (map.size() < k-1) map.put(n, 1);
-            else {
+                //there are at most k-1 elements that could have more than n/k occurrences in an array,
+                // for example, at most 2 distinct number could have a frequency of 1/3 in an array.
+            else if (map.size() < k - 1) {
+                map.put(n, 1);
+            } else {
                 boolean flag = false;
                 for (Integer i : map.keySet()) {
                     if (map.get(i) == 0) {
@@ -75,13 +68,10 @@ public class MajorityElement {
             if (map.get(i) > nums.length / k) list.add(i);
         }
         return list;
-
-
     }
 
-
     public static void main(String[] args) {
-        int[] arr = new int[]{1,1,1,1,2,3,3,2,2,2,3};
+        int[] arr = new int[]{1, 1, 1, 1, 2, 3, 3, 2, 2, 2, 3};
         System.out.print(new MajorityElement().majorityElement(arr));
     }
 
