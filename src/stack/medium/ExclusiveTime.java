cf49bff4e053f3d7755731dff596fc3fb02cdb27
poorvank <puravbhatia9@gmail.com>
2019-11-21 01:43:35 +0530

    Refactor: Exclusive Time.

--- src/stack/medium/ExclusiveTime.java
+++ src/stack/medium/ExclusiveTime.java
@@ -45,53 +45,34 @@ import java.util.Stack;
 public class ExclusiveTime {
 
     private class Function {
-
         public int id;
+        public boolean isStart;
         public int time;
-        public String action;
-
-        public Function(int id, int time,String action) {
-            this.id = id;
-            this.time = time;
-            this.action = action;
+        Function(String content) {
+            String[] s = content.split(":");
+            id = Integer.valueOf(s[0]);
+            isStart = s[1].equals("start");
+            time = Integer.valueOf(s[2]);
         }
     }
 
     public int[] exclusiveTime(int n, List<String> logs) {
-
-        if(n==0 || logs==null || logs.isEmpty()) {
-            return new int[]{};
-        }
-
-        int[] result = new int[n];
-
         Stack<Function> stack = new Stack<>();
-
-        int last=0;
-
-        for (String log : logs) {
-            String[] splitLog = log.split(":");
-            int id = Integer.parseInt(splitLog[0]);
-            int time = Integer.parseInt(splitLog[2]);
-            String action = splitLog[1];
-            Function f = new Function(id, time, action);
-
-            if (!stack.isEmpty() && stack.peek().id == f.id && stack.peek().action.equals("start") && f.action.equals("end")) {
-                result[stack.pop().id] += f.time - last + 1;
-                // Next interval will start with +1
-                last = f.time + 1;
-            } else {
-                if (!stack.isEmpty()) {
-                    result[stack.peek().id] += f.time - last;
-                }
+        int[] result = new int[n];
+        for (String content : logs) {
+            Function f = new Function(content);
+            if (f.isStart) {
                 stack.push(f);
-                last = f.time;
+            } else {
+                Function top = stack.pop();
+                result[top.id] += (f.time - top.time + 1);
+                if (!stack.isEmpty()) {
+                    result[stack.peek().id] -= (f.time - top.time + 1);
+                }
             }
-
         }
 
         return result;
-
     }
 
     public static void main(String[] args) {
@@ -104,3 +85,15 @@ public class ExclusiveTime {
     }
 
 }
+
+/*
+
+0:start:0" means function 0 starts from the very beginning of time 0.
+"0:end:0" means function 0 ends to the very end of time 0
+you need to read the question carefully.
+the last number of the log means a time segment，
+if timestamp is 0,it means a time segment [0,1]
+"0:start:0" means function 0 starts from the very beginning of time 0.
+"0:end:0" means function 0 ends to the very end of time 0，also means the very beginning of time 1.
+
+ */
