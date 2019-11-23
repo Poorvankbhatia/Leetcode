2062d2f0ee071b5eadec3200fd7fd95a0584d575
poorvank <puravbhatia9@gmail.com>
2019-11-23 12:15:25 +0530

    Last Stone Weight 2

--- src/dyanamicprogramming/medium/LastStoneWeightII.java
+++ src/dyanamicprogramming/medium/LastStoneWeightII.java
@@ -1,20 +1,82 @@
-package dyanamicprogramming.medium;
+/*
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+We have a collection of rocks, each rock has a positive integer weight.
+
+Each turn, we choose any two rocks and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the smallest possible weight of this stone (the weight is 0 if there are no stones left.)
+
+
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation:
+We can combine 2 and 4 to get 2 so the array converts to [2,7,1,8,1] then,
+we can combine 7 and 8 to get 1 so the array converts to [2,1,1,1] then,
+we can combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we can combine 1 and 1 to get 0 so the array converts to [1] then that's the optimal value.
+
+
+Note:
+
+1 <= stones.length <= 30
+1 <= stones[i] <= 100
+
+ */
+package dyanamicprogramming.medium;
 
 public class LastStoneWeightII {
 
     public int lastStoneWeightII(int[] stones) {
-        Map<String,Integer> map = new HashMap<>();
-        Arrays.sort(stones);
-        return -1;
-    }
+        int n = stones.length;
+        int sum=0;
+        for(int stone : stones) {
+            sum+=stone;
+        }
+        boolean[][] dp =new boolean[n+1][sum+1];
+        int max= 0;
+        for(int i=0;i<dp.length;i++) {
+            for(int j=0;j<=sum/2;j++) {
+                if(i==0 && j==0) {
+                    dp[i][j]=true;
+                } else if(i==0) {
+                    dp[i][j]=false;
+                } else if(j==0) {
+                    dp[i][j]=true;
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                    if(stones[i-1]<=j) {
+                        dp[i][j] |= (dp[i-1][j-stones[i-1]]);
+                    }
+                }
+                if(dp[i][j]) {
+                    max = Math.max(max,j);
+                }
+            }
+        }
 
-    private int util(int[] stones) {
-        return -1;
+        return sum-(2*max);
+
     }
 
 }
+
+/*
+
+This question eaquals to partition an array into 2 subsets whose difference is minimal
+(1) S1 + S2  = S
+(2) S1 - S2 = diff
+
+==> -> diff = S - 2 * S2  ==> minimize diff equals to  maximize S2
+
+Now we should find the maximum of S2 , range from 0 to S / 2, using dp can solve this
+
+dp[i][j]   = {true if some subset from 1st to j'th has a sum equal to sum i, false otherwise}
+    i ranges from (sum of all elements) {1..n}
+    j ranges from  {1..n}
+
+ */
