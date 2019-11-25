ff00316a88e44144347f0f96a1afdda92ce9d06d
poorvank <puravbhatia9@gmail.com>
2019-11-25 11:26:29 +0530

    Refactor: Search Insert.

--- src/binarysearch/medium/SearchInsert.java
+++ src/binarysearch/medium/SearchInsert.java
@@ -20,6 +20,40 @@ package binarysearch.medium;
 public class SearchInsert {
 
     public int searchInsert(int[] nums, int target) {
+        if(nums[nums.length-1]<target) {
+            return nums.length;
+        } else if(nums[0]>target) {
+            return 0;
+        }
+        int lo=0;
+        int hi=nums.length-1;
+        while(lo<hi) {
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target) {
+                return mid;
+            } else if(nums[mid]>target) {
+                hi=mid;
+            } else {
+                lo=mid+1;
+            }
+        }
+        return lo;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1,3,5,6};
+        System.out.println(new SearchInsert().searchInsert(nums,0));
+
+    }
+
+}
+
+/*
+
+Recursive:
+public int searchInsert(int[] nums, int target) {
 
         return posBinarySearch(nums,target,0,nums.length-1);
 
@@ -51,38 +85,6 @@ public class SearchInsert {
 
 
 
-
-    }
-
-    public static void main(String[] args) {
-
-        int[] nums = new int[]{1,3,5,6};
-        System.out.println(new SearchInsert().searchInsert(nums,0));
-
-    }
-
-}
-
-/*
-
-Another soln:
-
-public int searchInsert(int[] nums, int target) {
-
-        int low=0,high=nums.length-1;
-
-        while(low<=high) {
-            int mid = low+(high-low)/2;
-            if(nums[mid]==target) {
-                return mid;
-            } else if(nums[mid]<target) {
-                low=mid+1;
-            } else {
-                high=mid;
-            }
-        }
-
-        return low;
 
     }
 
