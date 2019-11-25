0978fdb15b60d6b81b100c46c02eeed2fb19b208
poorvank <puravbhatia9@gmail.com>
2019-11-25 11:15:09 +0530

    Refactor: Largest Sum of Averages.

--- src/dyanamicprogramming/medium/LargestSumOfAverages.java
+++ src/dyanamicprogramming/medium/LargestSumOfAverages.java
@@ -26,12 +26,95 @@ Answers within 10^-6 of the correct answer will be accepted as correct.
  */
 package dyanamicprogramming.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by poorvank.b on 08/04/18.
  */
 public class LargestSumOfAverages {
 
+    Map<String,Double> map;
     public double largestSumOfAverages(int[] A, int K) {
+        map = new HashMap<>();
+        return util(A,K,0,A.length-1);
+    }
+
+    private double util(int[] A,int K,int start,int end) {
+        String key = start+"_"+end+"_"+K;
+        if(map.containsKey(key)) {
+            return map.get(key);
+        }
+        if(K==1) {
+            double sum = 0;
+            for(int i=start;i<=end;i++) {
+                sum+=A[i];
+            }
+            double avg = sum/(end-start+1);
+            map.put(key,avg);
+            return avg;
+        }
+        double sum=0.0;
+        double max = 0.0;
+        for(int i=start;i<end;i++) {
+            sum+=A[i];
+            double avg = (sum/(i-start+1));
+            avg += util(A,K-1,i+1,end);
+            max = Math.max(avg,max);
+        }
+        map.put(key,max);
+        return max;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] A = new int[]{4663,3020,7789,1627,9668,1356,4207,1133,8765,4649,205,6455,8864,3554,3916,5925,3995,4540,3487,5444,8259,8802,
+                6777,7306,989,4958,2921,8155,4922,2469,6923,776,9777,1796,708,786,3158,7369,8715,2136,2510,3739,6411,7996,6211,8282,4805,236,1489,7698};
+        System.out.println(new LargestSumOfAverages().largestSumOfAverages(A,27));
+    }
+
+}
+
+/*
+
+Using 3D array:
+
+Double[][][] dp;
+    public double largestSumOfAverages(int[] A, int K) {
+        dp = new Double[100][100][100];
+        return util(A,K,0,A.length-1);
+    }
+
+    private double util(int[] A,int K,int start,int end) {
+        if(dp[start][end][K]!=null) {
+            return dp[start][end][K];
+        }
+        if(K==1) {
+            double sum = 0;
+            for(int i=start;i<=end;i++) {
+                sum+=A[i];
+            }
+            double avg = sum/(end-start+1);
+            dp[start][end][K]=avg;
+            return avg;
+        }
+        double sum=0.0;
+        double max = 0.0;
+        for(int i=start;i<end;i++) {
+            sum+=A[i];
+            double avg = (sum/(i-start+1));
+            avg += util(A,K-1,i+1,end);
+            max = Math.max(avg,max);
+        }
+        dp[start][end][K]=max;
+        return max;
+    }
+
+----------
+
+Using n(element count) & K as key:
+public double largestSumOfAverages(int[] A, int K) {
         int N = A.length;
         double[][] memo = new double[N+1][N+1];
         double cur = 0;
@@ -52,10 +135,7 @@ public class LargestSumOfAverages {
         return memo[n][k];
     }
 
-    public static void main(String[] args) {
-        int[] A = new int[]{4663,3020,7789,1627,9668,1356,4207,1133,8765,4649,205,6455,8864,3554,3916,5925,3995,4540,3487,5444,8259,8802,
-                6777,7306,989,4958,2921,8155,4922,2469,6923,776,9777,1796,708,786,3158,7369,8715,2136,2510,3739,6411,7996,6211,8282,4805,236,1489,7698};
-        System.out.println(new LargestSumOfAverages().largestSumOfAverages(A,27));
-    }
 
-}
+
+
+ */
