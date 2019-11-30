fa4735f39301afde70e080d81f980ccf5b812dc2
poorvank <puravbhatia9@gmail.com>
2019-11-30 16:16:43 +0530

    Refactor: Matrix01

--- src/bfsdfs/medium/Matrix01.java
+++ src/bfsdfs/medium/Matrix01.java
@@ -38,76 +38,41 @@ import java.util.*;
  */
 public class Matrix01 {
 
-    private class Cell {
-        int x,y,value;
-
-        public Cell(int x, int y,int value) {
-            this.x = x;
-            this.y = y;
-            this.value = value;
-        }
-    }
-
-    private int[] xMove = {0,1,-1,0};
-    private int[] yMove = {1,0,0,-1};
-
-    public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        int m = matrix.size();
-        if(m==0) {
-            return result;
-        }
-        int n  = matrix.get(0).size();
-
-        Integer[][] resultArr = new Integer[m][n];
-
-
-        Queue<Cell> queue = new LinkedList<>();
-        for (int i=0;i<m;i++) {
-            for (int j=0;j<n;j++) {
-                if(matrix.get(i).get(j)==0) {
-                    resultArr[i][j] = 0;
-                    Cell cell = new Cell(i,j,0);
-                    queue.add(cell);
+    int[][] dir = new int[][]{{0,1},{0,-1},{-1,0},{1,0}};
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] result = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(matrix[i][j]==0) {
+                    queue.add(new int[]{i,j,0});
                 } else {
-                    resultArr[i][j] = Integer.MAX_VALUE;
+                    result[i][j]=Integer.MAX_VALUE;
                 }
-
             }
         }
-
         while (!queue.isEmpty()) {
-
-            Cell current = queue.poll();
+            int[] poll = queue.poll();
             for (int i=0;i<4;i++) {
-                int nextX = current.x+xMove[i];
-                int nextY = current.y+yMove[i];
-                if(nextX>=0 && nextX<m && nextY>=0 && nextY<n) {
-                   Cell newCell;
-                   //Only add into queue if value is smaller than currently calculated distance
-                   if(resultArr[nextX][nextY]>current.value+1) {
-                       resultArr[nextX][nextY] = current.value+1;
-                       newCell = new Cell(nextX,nextY,current.value+1);
-                       queue.add(newCell);
-                   }
+                int nextX = poll[0]+dir[i][0];
+                int nextY = poll[1]+dir[i][1];
+                if(nextX>=0 && nextY>=0 && nextX<m && nextY<n) { // No visited array required as some other 0 can also be close to it.
+                    if(result[nextX][nextY]>poll[2]+1) {
+                        result[nextX][nextY] = poll[2]+1;
+                        queue.add(new int[]{nextX,nextY,poll[2]+1});
+                    }
                 }
             }
-
         }
-
-        for (Integer[] array : resultArr) {
-            result.add(Arrays.asList(array));
-        }
-
         return result;
     }
-
 }
 
 /*
 
-Simple BFS
+Simple BFS.
+Walls And Gates.
 
  */
