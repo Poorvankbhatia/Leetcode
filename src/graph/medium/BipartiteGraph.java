a4c3f77e23330a58a5f67e97aaedbfaead04de8c
poorvank <puravbhatia9@gmail.com>
2019-11-30 12:13:39 +0530

    Refactor: Bipartite Graph.

--- src/graph/medium/BipartiteGraph.java
+++ src/graph/medium/BipartiteGraph.java
@@ -99,3 +99,38 @@ public class BipartiteGraph {
     }
 
 }
+
+/*
+
+Better Approach:
+
+public boolean isBipartite(int[][] graph) {
+        //BFS
+        // 0(not meet), 1(black), 2(white)
+        int[] visited = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[i].length != 0 && visited[i] == 0) {
+                visited[i] = 1;
+                Queue<Integer> q = new LinkedList<>();
+                q.offer(i);
+                while(! q.isEmpty()) {
+                    int current = q.poll();
+                    for (int c: graph[current]) {
+
+                        if (visited[c] == 0) {
+                            visited[c] = (visited[current] == 1) ? 2 : 1;
+                            q.offer(c);
+                        } else {
+                            if (visited[c] == visited[current]) return false;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return true;
+    }
+
+ */
