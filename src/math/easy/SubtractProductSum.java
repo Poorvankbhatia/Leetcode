5304173b8414ed967d62e2f614db1b6bde45a228
poorvank <puravbhatia9@gmail.com>
2019-12-08 12:42:50 +0530

    SubtractProductSum

---
+++ src/math/easy/SubtractProductSum.java
@@ -0,0 +1,46 @@
+/*
+
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+
+Example 1:
+
+Input: n = 234
+Output: 15
+Explanation:
+Product of digits = 2 * 3 * 4 = 24
+Sum of digits = 2 + 3 + 4 = 9
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation:
+Product of digits = 4 * 4 * 2 * 1 = 32
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+
+
+Constraints:
+
+1 <= n <= 10^5
+
+ */
+package math.easy;
+
+public class SubtractProductSum {
+
+    public int subtractProductAndSum(int n) {
+        int sum=0;
+        int product=1;
+        while(n!=0) {
+            int x = n%10;
+            sum+=x;
+            product*=x;
+            n=n/10;
+        }
+        return product-sum;
+    }
+
+
+}
