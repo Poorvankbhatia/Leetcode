f185a3322990b05bf2e07408a428294a0d406aa8
poorvank <puravbhatia9@gmail.com>
2019-12-09 10:23:48 +0530

    Refactor: Search Rotated Array

--- src/binarysearch/medium/SearchRotatedArray.java
+++ src/binarysearch/medium/SearchRotatedArray.java
@@ -18,53 +18,50 @@ package binarysearch.medium;
 public class SearchRotatedArray {
 
     public int search(int[] nums, int target) {
-
-        int pivot = findPivot(nums,0,nums.length-1);
-
-        int result = binarySearch(nums,nums.length-1,pivot,target);
-        if(result==-1) {
-            return binarySearch(nums,pivot-1,0,target);
+        if(nums.length==0) {
+            return -1;
         }
-
-        return result;
+        int pivot = findPivot(nums);
+        //System.out.println(pivot);
+        int val = bs(0,pivot,nums,target);
+        if(val==-1) {
+            return bs(pivot+1,nums.length-1,nums,target);
+        }
+        return val;
     }
 
-    private int findPivot(int[] nums,int low,int high) {
-
-        if(nums[low]<=nums[high]) {
-            return low;
-        }
-
-        int mid = low + (high-low)/2;
-
-        if((mid==0 || nums[mid]<nums[mid-1]) && (mid==nums.length-1 || nums[mid]<nums[mid+1])) {
-            return mid;
-        } else if(nums[mid]>nums[high]) {
-            return findPivot(nums,mid+1,high);
-        }
-
-        return findPivot(nums,low,mid-1);
-    }
-
-    private int binarySearch(int[] nums,int high,int low,int target) {
-
-        if(high>=low) {
-
-            int mid = (low) + (high - low) / 2;
-
-            if (nums[mid] == target) {
+    private int findPivot(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<end) {
+            int mid = start+(end-start)/2;
+            if((mid==0 || nums[mid]>nums[mid-1]) && (mid== nums.length-1 || nums[mid]>nums[mid+1])) {
                 return mid;
-            } else if (nums[mid] > target) {
-                return binarySearch(nums, mid - 1, low, target);
             }
-
-            return binarySearch(nums, high, mid + 1, target);
+            if(nums[mid]<nums[nums.length-1]) {
+                end=mid-1;
+            } else {
+                start=mid+1;
+            }
         }
-
-
-        return -1;
-
+        return start;
     }
+
+    private int bs(int start,int end,int[] nums,int target) {
+        while(start<=end) {
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }
+            if(nums[mid]>target) {
+                end=mid-1;
+            } else {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
         int[] arr = new int[]{5,1,3};
