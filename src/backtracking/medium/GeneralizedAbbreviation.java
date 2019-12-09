ccb36bea4be56abdec487364cd2cd340489b87dd
poorvank <puravbhatia9@gmail.com>
2019-12-09 14:59:55 +0530

    Refactor: Generalised Abbreviation.

--- src/backtracking/medium/GeneralizedAbbreviation.java
+++ src/backtracking/medium/GeneralizedAbbreviation.java
@@ -11,7 +11,7 @@ Output:
 ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
 
  */
-package string.medium;
+package backtracking.medium;
 
 import java.util.ArrayList;
 import java.util.List;
