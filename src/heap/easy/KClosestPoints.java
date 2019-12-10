4117cf312de0ab2d5eaea69241eb6b19d47e5643
poorvank <puravbhatia9@gmail.com>
2019-12-10 13:12:01 +0530

    Refactor: K Closest points.

--- src/heap/easy/KClosestPoints.java
+++ src/heap/easy/KClosestPoints.java
@@ -74,33 +74,41 @@ public class KClosestPoints {
 Using Quick Select:
 
 public int[][] kClosest(int[][] points, int K) {
-    int len =  points.length, l = 0, r = len - 1;
-    while (l <= r) {
-        int mid = helper(points, l, r);
-        if (mid == K) break;
-        if (mid < K) {
-            l = mid + 1;
-        } else {
-            r = mid - 1;
+        int len =  points.length, l = 0, r = len - 1;
+        while (l <= r) {
+            int mid = helper(points, l, r);
+            if (mid == K) break;
+            if (mid < K) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
+        return Arrays.copyOfRange(points, 0, K);
     }
-    return Arrays.copyOfRange(points, 0, K);
-}
 
-private int helper(int[][] A, int l, int r) {
-    int[] pivot = A[l];
-    while (l < r) {
-        while (l < r && compare(A[r], pivot) >= 0) r--;
-        A[l] = A[r];
-        while (l < r && compare(A[l], pivot) <= 0) l++;
-        A[r] = A[l];
+    private int helper(int[][] A, int start, int end) {
+
+        int left = start;
+        int[] pivot = A[end];
+        for (int i=start;i<end;i++) {
+            if(compare(A[i],pivot)<0) {
+                swap(A,left++,i);
+            }
+        }
+        swap(A,left,end);
+        return left;
     }
-    A[l] = pivot;
-    return l;
-}
 
-private int compare(int[] p1, int[] p2) {
-    return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+    private void swap(int[][] nums,int i1,int i2) {
+        int[] temp = nums[i1];
+        nums[i1]=nums[i2];
+        nums[i2]=temp;
+    }
+
+    private int compare(int[] p1, int[] p2) {
+        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+    }
 }
 
 
