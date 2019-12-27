fc93fea9a0bbc6917daef61a90010d932dfda304
poorvank <puravbhatia9@gmail.com>
2019-12-27 22:54:24 +0530

    changes

--- src/heap/hard/BuildingBlocks.java
+++ src/heap/hard/BuildingBlocks.java
@@ -4,7 +4,7 @@ You are given a list of blocks, where blocks[i] = t means that the i-th block ne
 
 A worker can either split into two workers (number of workers increases by one) or build a block then go home. Both decisions cost some time.
 
-The time cost of spliting one worker into two workers is given as an integer split. Note that if two workers split at the same time,
+The time cost of splitting one worker into two workers is given as an integer split. Note that if two workers split at the same time,
 they split in parallel so the cost would be split.
 
 Output the minimum time needed to build all blocks.
