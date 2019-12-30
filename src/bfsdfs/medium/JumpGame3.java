188477b41c3fc643bb035d4d8d4dbd9c973b2f8a
poorvank <puravbhatia9@gmail.com>
2019-12-30 10:47:31 +0530

    Jump Game 3

---
+++ src/bfsdfs/medium/JumpGame3.java
@@ -0,0 +1,71 @@
+/*
+
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i,
+you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation:
+All possible ways to reach at index 3 with value 0 are:
+index 5 -> index 4 -> index 1 -> index 3
+index 5 -> index 6 -> index 4 -> index 1 -> index 3
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true
+Explanation:
+One possible way to reach at index 3 with value 0 is:
+index 0 -> index 4 -> index 1 -> index 3
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+
+
+Constraints:
+
+1 <= arr.length <= 5 * 10^4
+0 <= arr[i] < arr.length
+0 <= start < arr.length
+
+ */
+package bfsdfs.medium;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class JumpGame3 {
+
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        if(arr[start]==0) {
+            return true;
+        }
+        Set<Integer> visited = new HashSet<>();
+        visited.add(start);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        while(!queue.isEmpty()) {
+            int pop = queue.poll();
+            if(arr[pop]==0) {
+                return true;
+            }
+            if(pop+arr[pop]>=0 && pop+arr[pop]<n && visited.add(pop+arr[pop])) {
+                queue.add(pop+arr[pop]);
+            }
+            if(pop-arr[pop]>=0 && pop-arr[pop]<n && visited.add(pop-arr[pop])) {
+                queue.add(pop-arr[pop]);
+            }
+        }
+        return false;
+    }
+}
