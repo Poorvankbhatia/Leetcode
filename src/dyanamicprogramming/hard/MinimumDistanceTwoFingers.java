722303f45bd5e00bd049623f2322bb5526da9ae3
poorvank <puravbhatia9@gmail.com>
2020-01-12 15:58:00 +0530

    Minimum Distance 2 fingers.

--- src/dyanamicprogramming/hard/MinimumDistanceTwoFingers.java
+++ src/dyanamicprogramming/hard/MinimumDistanceTwoFingers.java
@@ -54,26 +54,40 @@ Each word[i] is an English uppercase letter.
 package dyanamicprogramming.hard;
 public class MinimumDistanceTwoFingers {
 
-    public int minimumDistance(String str) {
-        char[] S = str.toCharArray();
-        int N = S.length;
-        int ans = Integer.MAX_VALUE;
-        int[][][] dp = new int[26][26][N + 1];
-        for(int z = N-1; z >= 0; --z){
-            for(int x = 0; x < 26; ++x){
-                for(int y = 0; y < 26; ++y){
-                    dp[x][y][z] = Math.min(cost(x,S[z]-'A')+dp[S[z]-'A'][y][z+1], cost(y,S[z]-'A')+dp[x][S[z]-'A'][z+1]);
-                    if (z == 0){
-                        ans = Math.min(ans, dp[x][y][z]);
-                    }
-                }
-            }
-        }
-        return ans;
+    int[][][] memo = new int[27][27][300];
+
+    public int minimumDistance(String word) {
+        return minDist(word, 0, null, null);
     }
 
-    private int cost(int x, int y){
-        return Math.abs(x/6-y/6)+Math.abs(x%6-y%6);
+    private int minDist(String word, int pos, Character c1, Character c2) {
+        if (pos >= word.length())
+            return 0;
+        int idx1 = c1 == null ? 0 : c1 - 'A' + 1;
+        int idx2 = c2 == null ? 0 : c2 - 'A' + 1;
+        if (memo[idx1][idx2][pos] == 0) {
+            memo[idx1][idx2][pos] = Math.min(getDist(c1,word.charAt(pos)) + minDist(word,pos+1,word.charAt(pos),c2),
+                    getDist(c2,word.charAt(pos)) + minDist(word,pos+1,c1,word.charAt(pos)));
+        }
+        return memo[idx1][idx2][pos];
+    }
+
+    private int getDist(Character c1, Character c2) {
+        if (c1 == null || c2 == null) return 0;
+        int d1 = c1 - 'A', d2 = c2 - 'A';
+        int x1 = d1 / 6, y1 = d1 % 6;
+        int x2 = d2 / 6, y2 = d2 % 6;
+        return Math.abs(x1-x2) + Math.abs(y1-y2);
     }
 
 }
+
+/*
+
+Suppose we have dp[x][y][z] where x represents position our first hand and y represents position of second hand and z represents current index.
+So dp[x][y][z]=min(distance from x to S[z] + dp[S[z]][y][z+1],distance from y to S[z] + dp[x][S[z]][z+1]), where S is the given string.
+Base case : dp[x][y][z]=0 when z = length of string.
+Return min(dp[i][j][0] where i=[0,25] and j=[0,25]).
+Complexity will be O(2626N), where N will be the size of the string.
+
+ */
