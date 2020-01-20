52087bdfe1a89a779a6d1389b47a99a5678e8f41
poorvank <puravbhatia9@gmail.com>
2020-01-20 13:00:23 +0530

    Refactor:

--- src/arrays/medium/RemoveCoveredIntervals.java
+++ src/arrays/medium/RemoveCoveredIntervals.java
@@ -28,21 +28,17 @@ import java.util.Arrays;
 public class RemoveCoveredIntervals {
 
     public int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)->(a[0]-b[0]!=0?a[0]-b[0]:b[1]-a[1]));
-        int n = intervals.length;
-        int[] first = intervals[0];
-        int count=1;
-        for(int i=1;i<n;i++) {
-            if(!covers(first,intervals[i])) {
-                first = intervals[i];
+        Arrays.sort(intervals,(a,b)->(a[0]!=b[0])?a[0]-b[0]:b[1]-a[1]);
+        int count=0;
+        int[] compare = new int[]{intervals[0][0],intervals[0][1]};
+        for(int i=1;i<intervals.length;i++) {
+            if(intervals[i][0]>=compare[0] && intervals[i][1]<=compare[1]) {
                 count++;
             }
+            compare[0] = Math.min(compare[0],intervals[i][0]);
+            compare[1] = Math.max(compare[1],intervals[i][1]);
         }
-        return count;
-    }
-
-    private boolean covers(int[] a,int[] b) {
-        return a[0]<=b[0] && a[1]>=b[1];
+        return intervals.length-count;
     }
 
     public static void main(String[] args) {
