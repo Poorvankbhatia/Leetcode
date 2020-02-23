ecd33c90b900553b8a7b738aff5096a6618d8a0b
poorvank <poorvank@amazon.com>
2020-02-23 19:39:50 +0530

    Largest Multiple of 3 Validate BT Nodes.

---
+++ src/math/hard/LargestMultipleOfThree.java
@@ -0,0 +1,143 @@
+/*
+Given an integer array of digits, return the largest multiple of three that can be formed by concatenating some of the given digits in any order.
+
+Since the answer may not fit in an integer data type, return the answer as a string.
+
+If there is no answer return an empty string.
+
+
+
+Example 1:
+
+Input: digits = [8,1,9]
+Output: "981"
+Example 2:
+
+Input: digits = [8,6,7,1,0]
+Output: "8760"
+Example 3:
+
+Input: digits = [1]
+Output: ""
+Example 4:
+
+Input: digits = [0,0,0,0,0,0]
+Output: "0"
+
+
+Constraints:
+
+1 <= digits.length <= 10^4
+0 <= digits[i] <= 9
+The returning answer must not contain unnecessary leading zeros.
+ */
+package math.hard;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LargestMultipleOfThree {
+
+    public String largestMultipleOfThree(int[] arr) {
+        Arrays.sort(arr);
+        /*
+        Take three queues.
+        queue0 for storing elements which on dividing by 3 gives remainder as 0.
+        queue1 stores digits which on dividing by 3 gives remainder as 1.
+        queue2 stores digits which on dividing by 3 gives remainder as 2.
+         */
+        Queue<Integer> queue0=new LinkedList<>();
+        Queue<Integer> queue1=new LinkedList<>();
+        Queue<Integer> queue2=new LinkedList<>();
+        int sum=0;
+        for (int a : arr) {
+            sum += a;
+            if (a % 3 == 0) {
+                queue0.add(a);
+            }
+            else if (a % 3 == 1) {
+                queue1.add(a);
+            }
+            else {
+                queue2.add(a);
+            }
+        }
+        if(sum==0) {
+            return "0";
+        }
+        /*
+        Sum of digits produces remainder 1 when divided by 3.
+        Remove one item from queue1.
+        If queue1 is empty, remove two items from queue2.
+        If queue2 contains less than two items, the number is not possible.
+         */
+        if ((sum % 3) == 1) {
+            if (!queue1.isEmpty()) {
+                queue1.remove();
+            }
+            else {
+                if (queue2.size() < 2) {
+                    return "";
+                }
+                queue2.remove();
+                queue2.remove();
+
+            }
+        }
+        /*
+        Sum of digits produces remainder 2 when divided by 3.
+        Remove one item from queue2.
+        If queue2 is empty, remove two items from queue1.
+        If queue1 contains less than two items, the number is not possible.
+         */
+        else if (sum % 3 == 2)
+        {
+            if (!queue2.isEmpty()) {
+                queue2.remove();
+            }
+            else {
+                if(queue1.size()<2) {
+                    return "";
+                }
+                queue1.remove();
+                queue1.remove();
+            }
+        }
+
+        int[] temp = new int[arr.length];
+        int top= populate(temp,queue0,queue1,queue2);
+        Arrays.sort(temp,0,top);
+
+        StringBuilder result = new StringBuilder();
+        for (int i = top-1; i>=0; i--) {
+            result.append(temp[i]);
+        }
+
+        return result.toString();
+
+    }
+
+    private int populate(int[] temp, Queue<Integer> queue0, Queue<Integer> queue1, Queue<Integer> queue2) {
+        int top=0;
+        while(!queue0.isEmpty()) {
+            temp[top++] = queue0.remove();
+        }
+        while(!queue1.isEmpty()) {
+            temp[top++]=queue1.remove();
+        }
+        while(!queue2.isEmpty()) {
+            temp[top++]=queue2.remove();
+        }
+        return top;
+    }
+
+}
+
+/*
+
+G I
+
+
+
+ */
--- src/tree/medium/ValidateBTNodes.java
+++ src/tree/medium/ValidateBTNodes.java
@@ -28,28 +28,36 @@ leftChild.length == rightChild.length == n
 package tree.medium;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ValidateBTNodes {
 
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
 
+        Set<Integer>[] childArr = new Set[n];
         int[] parent = new int[n];
         Arrays.fill(parent,-1);
+        for (int i=0;i<n;i++) {
+            childArr[i]= new HashSet<>();
+        }
 
         for (int i=0;i<n;i++) {
             if(leftChild[i]!=-1) {
-                // If parent already assigned.
-                if(parent[leftChild[i]]!=-1) {
+                // If parent already assigned or leftChild[i] is already a grand parent of i;
+                if(parent[leftChild[i]]!=-1 || isCurrentChildAPreviousParent(leftChild[i],childArr,i)) {
                     return false;
                 }
                 parent[leftChild[i]]=i;
+                childArr[i].add(leftChild[i]);
             }
             if(rightChild[i]!=-1) {
-                // If parent already assigned.
-                if(parent[rightChild[i]]!=-1) {
+                // If parent already assigned or rightChild[i] is already a grand parent of i;
+                if(parent[rightChild[i]]!=-1 || isCurrentChildAPreviousParent(rightChild[i],childArr,i)) {
                     return false;
                 }
                 parent[rightChild[i]]=i;
+                childArr[i].add(rightChild[i]);
             }
         }
 
@@ -62,6 +70,24 @@ public class ValidateBTNodes {
         }
 
         return rootCount==1;
+    }
+
+    // Check if the child is a grand parent of the current index
+    private boolean isCurrentChildAPreviousParent(int child, Set<Integer>[] childArr, int parent) {
+        if(child==parent) {
+            return true;
+        }
+        for (Integer c : childArr[child]) {
+            boolean v =  isCurrentChildAPreviousParent(c,childArr,parent);
+            if(v) {
+                return v;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidateBTNodes().validateBinaryTreeNodes(4,new int[]{1, 2, 0, -1},new int[]{-1,-1,-1,-1}));
     }
 
 }
