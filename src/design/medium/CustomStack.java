f74a9e4e1a023a41ebfad4768f126f879acc2c75
poorvank <poorvank@amazon.com>
2020-03-21 15:34:10 +0530

    Custom Stack.

---
+++ src/design/medium/CustomStack.java
@@ -0,0 +1,115 @@
+/*
+Design a stack which supports the following operations.
+
+Implement the CustomStack class:
+
+CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack or do nothing if the stack reached the maxSize.
+void push(int x) Adds x to the top of the stack if the stack hasn't reached the maxSize.
+int pop() Pops and returns the top of stack or -1 if the stack is empty.
+void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, just increment all the elements in the stack.
+
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack customStack = new CustomStack(3); // Stack is Empty []
+customStack.push(1);                          // stack becomes [1]
+customStack.push(2);                          // stack becomes [1, 2]
+customStack.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+customStack.push(2);                          // stack becomes [1, 2]
+customStack.push(3);                          // stack becomes [1, 2, 3]
+customStack.push(4);                          // stack still [1, 2, 3], Don't add another elements as size is 4
+customStack.increment(5, 100);                // stack becomes [101, 102, 103]
+customStack.increment(2, 100);                // stack becomes [201, 202, 103]
+customStack.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+customStack.pop();                            // return 202 --> Return top of the stack 102, stack becomes [201]
+customStack.pop();                            // return 201 --> Return top of the stack 101, stack becomes []
+customStack.pop();                            // return -1 --> Stack is empty return -1.
+
+
+Constraints:
+
+1 <= maxSize <= 1000
+1 <= x <= 1000
+1 <= k <= 1000
+0 <= val <= 100
+At most 1000 calls will be made to each method of increment, push and pop each separately.
+ */
+package design.medium;
+
+import java.util.*;
+
+public class CustomStack {
+
+    private List<Integer> stack = new ArrayList<>();
+    private int maxSize;
+    public CustomStack(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void push(int x) {
+        if (stack.size() < maxSize) {
+            stack.add(x);
+        }
+    }
+
+    public int pop() {
+        return stack.isEmpty() ? -1 : stack.remove(stack.size() - 1);
+    }
+
+    public void increment(int k, int val) {
+        for (int i = 0; i < k && i < stack.size(); ++i) {
+            stack.set(i, stack.get(i) + val);
+        }
+    }
+
+}
+
+/*
+
+Another Method:
+
+
+Use an additional array to record the increment value.
+inc[i] means for all elements stack[0] ~ stack[i],
+we should plus inc[i] when popped from the stack.
+Then inc[i-1]+=inc[i], so that we will keep it from next pop.
+
+
+     int n;
+    int[] inc;
+    Stack<Integer> stack;
+    public CustomStack(int maxSize) {
+        n = maxSize;
+        inc = new int[n];
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (stack.size() < n)
+            stack.push(x);
+    }
+
+    public int pop() {
+        int i = stack.size() - 1;
+        if (i < 0)
+            return -1;
+        if (i > 0)
+            inc[i - 1] += inc[i];
+        int res = stack.pop() + inc[i];
+        inc[i] = 0;
+        return res;
+    }
+
+    public void increment(int k, int val) {
+        int i = Math.min(k, stack.size()) - 1;
+        if (i >= 0)
+            inc[i] += val;
+    }
+
+ */
