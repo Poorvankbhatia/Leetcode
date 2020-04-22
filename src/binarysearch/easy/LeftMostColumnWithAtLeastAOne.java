cd954437a087e32efe13a67ceba077c7f8575ef4
poorvank <poorvank@amazon.com>
2020-04-22 20:53:30 +0530

    Left Most Column with Atleast A One.

---
+++ src/binarysearch/easy/LeftMostColumnWithAtLeastAOne.java
@@ -0,0 +1,49 @@
+/*
+A binary matrix means that all elements are 0 or 1. For each individual row of the matrix, this row is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return leftmost column index(0-indexed) with at least a 1 in it. If such index doesn't exist, return -1.
+
+You can't access the Binary Matrix directly.  You may only access the matrix using a BinaryMatrix interface:
+
+BinaryMatrix.get(x, y) returns the element of the matrix at index (x, y) (0-indexed).
+BinaryMatrix.dimensions() returns a list of 2 elements [m, n], which means the matrix is m * n.
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer.  Also, any solutions that attempt to
+circumvent the judge will result in disqualification.
+
+For custom testing purposes you're given the binary matrix mat as input in the following four examples. You will not have access the binary matrix directly.
+
+Input: mat = [[0,0,0,1],[0,0,1,1],[0,1,1,1]]
+Output: 1
+
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+mat[i][j] is either 0 or 1.
+mat[i] is sorted in a non-decreasing way.
+ */
+package binarysearch.easy;
+
+import java.util.List;
+public class LeftMostColumnWithAtLeastAOne {
+
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> list = binaryMatrix.dimensions();
+        int rows = list.get(0);
+        int cols = list.get(1);
+        int candidate = -1;
+        for (int r = 0, c = cols - 1; r < rows && c >= 0; ) {
+            if (binaryMatrix.get(r,c) == 1) {
+                candidate = c;
+                c--;
+            } else {
+                r++;
+            }
+        }
+        return candidate;
+    }
+
+
+}
