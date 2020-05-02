e01cf986e8644cb82abb2be663b962ea45915263
poorvank <poorvank@amazon.com>
2020-05-02 12:41:09 +0530

    Time Needed to inform (BFS added.)

--- src/bfsdfs/medium/TimeNeededToInform.java
+++ src/bfsdfs/medium/TimeNeededToInform.java
@@ -122,4 +122,23 @@ public class TimeNeededToInform {
          7  2        5(170)
                      |
                      0
+
+
+ BFS:
+
+ public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) if (manager[i] != -1) graph[manager[i]].add(i);
+        Queue<int[]> q = new LinkedList<>(); // Since it's a tree, we don't need `visited` array
+        q.offer(new int[]{headID, 0});
+        int ans = 0;
+        while (!q.isEmpty()) {
+            int[] top = q.poll();
+            int u = top[0], w = top[1];
+            ans = Math.max(w, ans);
+            for (int v : graph[u]) q.offer(new int[]{v, w + informTime[u]});
+        }
+        return ans;
+    }
  */
