b14e0efbfcaaf2d5bef4cdbe966d60666cc2d65f
Poorvank Bhatia <poorvank@uber.com>
2022-11-04 21:15:00 +0530

    MaxPointsWithCost

--- src/dyanamicprogramming/medium/MaxPointsWithCost.java
+++ src/dyanamicprogramming/medium/MaxPointsWithCost.java
@@ -48,15 +48,15 @@ public class MaxPointsWithCost {
         long ans = 0;
         int m = points.length, n = points[0].length;
         long[] dp = new long[n];
-        for (int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[j] += points[i][j];
+        for (int[] point : points) {
+            for (int j = 0; j < n; j++) {
+                dp[j] += point[j];
             }
-            for(int j=1;j<n;j++){
-                dp[j] = Math.max(dp[j], dp[j-1] - 1);
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.max(dp[j], dp[j - 1] - 1);
             }
-            for(int j=n-2;j>=0;j--){
-                dp[j] = Math.max(dp[j], dp[j+1] - 1);
+            for (int j = n - 2; j >= 0; j--) {
+                dp[j] = Math.max(dp[j], dp[j + 1] - 1);
             }
         }
         for(int i=0;i<n;i++){
