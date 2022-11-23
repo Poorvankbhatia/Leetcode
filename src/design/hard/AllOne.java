7378f533d743cf8998e11717c9954647138dc888
Poorvank Bhatia <poorvank@uber.com>
2022-11-23 23:18:12 +0530

    Refactor

--- src/design/hard/AllOne.java
+++ src/design/hard/AllOne.java
@@ -18,140 +18,169 @@ Challenge: Perform all these in O(1) time complexity.
 
 package design.hard;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by poorvank on 18/11/16.
  */
+class DLLNode {
+    int freq;
+    DLLNode prev;
+    DLLNode next;
+    Set<String> keys;
+
+    public DLLNode(int freq) {
+        this.freq = freq;
+        keys = new HashSet<>();
+    }
+}
+
+
 public class AllOne {
 
-    private class DLLNode {
-        String key;
-        int val;
-        DLLNode next;
-        DLLNode prev;
-
-        public DLLNode(String key,int val) {
-            this(key,val,null,null);
-        }
-
-        public DLLNode(String key,int val,DLLNode next,DLLNode prev) {
-            this.key = key;
-            this.val = val;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
-
-    DLLNode head,tail;
-    private HashMap<String,DLLNode> map;
+    private DLLNode head;
+    private DLLNode tail;
+    Map<String, DLLNode> map;
 
     /** Initialize your data structure here. */
     public AllOne() {
-        map = new HashMap<>();
         head = null;
         tail = null;
+        map = new HashMap<>();
     }
 
     /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
     public void inc(String key) {
-        if(map.containsKey(key)) {
-            DLLNode node = map.get(key);
-            node.val++;
-            if(node!=head && node.val>=head.val) {
-                delete(node);
-                makeHead(node);
+
+        if (map.containsKey(key)) {
+            DLLNode DLLNode = map.get(key);
+            int freq = DLLNode.freq;
+            DLLNode.keys.remove(key);
+            if (DLLNode.next == null) {
+                DLLNode newDLLNode = new DLLNode(freq + 1);
+                DLLNode.next = newDLLNode;
+                newDLLNode.prev = DLLNode;
+                newDLLNode.keys.add(key);
+                map.put(key, newDLLNode);
+                tail = newDLLNode;
+            } else {
+                DLLNode next = DLLNode.next;
+                if (next.freq - freq > 1) {
+                    DLLNode newDLLNode = new DLLNode(freq + 1);
+                    newDLLNode.keys.add(key);
+                    DLLNode.next = newDLLNode;
+                    newDLLNode.prev = DLLNode;
+                    newDLLNode.next = next;
+                    next.prev = newDLLNode;
+                    map.put(key, newDLLNode);
+                } else {
+                    next.keys.add(key);
+                    map.put(key, next);
+                }
             }
-        } else {
-            DLLNode node = new DLLNode(key,1);
-            map.put(key,node);
-            add(node);
+
+            if (DLLNode.keys.size() == 0) {
+                removeNode(DLLNode);
+            }
+
+        } else { // map does not contain the key
+            if (head == null) {
+                head = new DLLNode(1);
+                head.keys.add(key);
+                tail = head;
+            } else {
+                if (head.freq == 1) {
+                    head.keys.add(key);
+                } else {
+                    DLLNode newDLLNode = new DLLNode(1);
+                    newDLLNode.keys.add(key);
+                    newDLLNode.next = head;
+                    head.prev = newDLLNode;
+                    head = newDLLNode;
+                }
+            }
+            map.put(key, head);
         }
     }
 
     /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
     public void dec(String key) {
-        if(map.containsKey(key)) {
-            DLLNode node = map.get(key);
-            node.val--;
-            if(node.val==0) {
-                delete(node);
-                map.remove(key);
-            } else if(node!=tail && tail.val>=node.val) {
-                delete(node);
-                makeTail(node);
+        if (!map.containsKey(key)) {
+            return;
+        }
+        DLLNode DLLNode = map.get(key);
+        DLLNode.keys.remove(key);
+        int freq = DLLNode.freq;
+        if (freq == 1) {
+            map.remove(key);
+
+        } else if (DLLNode == head) {
+            DLLNode newDLLNode = new DLLNode(freq - 1);
+            newDLLNode.keys.add(key);
+            newDLLNode.next = head;
+            head.prev = newDLLNode;
+            head = newDLLNode;
+            map.put(key, head);
+        } else {
+            DLLNode prev = DLLNode.prev;
+            if (freq - prev.freq == 1) {
+                prev.keys.add(key);
+                map.put(key, prev);
+            } else {
+                DLLNode newDLLNode = new DLLNode(freq - 1);
+                prev.next = newDLLNode;
+                newDLLNode.prev = prev;
+                newDLLNode.next = DLLNode;
+                DLLNode.prev = newDLLNode;
+                newDLLNode.keys.add(key);
+                map.put(key, newDLLNode);
             }
+        }
+
+        if (DLLNode.keys.size() == 0) {
+            removeNode(DLLNode);
         }
     }
 
+
     /** Returns one of the keys with maximal value. */
     public String getMaxKey() {
-        if(head!=null){
-            return head.key;
+        if (head == null) {
+            return "";
         }
-        return "";
+        return tail.keys.iterator().next();
     }
 
     /** Returns one of the keys with Minimal value. */
     public String getMinKey() {
-        if(tail!=null){
-            return tail.key;
+        if (head == null) {
+            return "";
         }
-        return "";
+        return head.keys.iterator().next();
     }
 
-    private void delete(DLLNode node) {
 
-        if(head==tail) {
-            head =tail=null;
-        } else if(node==tail) {
-            tail = node.prev;
-            tail.next = null;
-        } else if(node==head) {
+    private void removeNode(DLLNode DLLNode) {
+        if (DLLNode == head) {
             head = head.next;
-            head.prev=null;
+            DLLNode.next = null;
+            if (head != null) {
+                head.prev = null;
+            }
+        } else if (DLLNode == tail) {
+            tail = tail.prev;
+            DLLNode.prev = null;
+            if (tail != null) {
+                tail.next = null;
+            }
         } else {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
+            DLLNode.prev.next = DLLNode.next;
+            DLLNode.next.prev = DLLNode.prev;
+            DLLNode.prev = null;
+            DLLNode.next = null;
         }
-
-    }
-
-    private void add(DLLNode node) {
-
-        if(head==null){
-            head = tail = node;
-        } else if(head == tail) {
-            head.next=node;
-            node.prev = head;
-            tail = node;
-        } else if(node.val<=tail.val) { // In case the newly added node is smaller than tail
-            tail.next = node;
-            node.prev = tail;
-            tail = node;
-        } else {
-            DLLNode next = head.next;
-            node.next = next;
-            next.prev = node;
-            node.prev = head;
-            head.next = node;
-        }
-
-    }
-
-    private void makeHead(DLLNode node) {
-
-        node.next = head;
-        head.prev = node;
-        head = node;
-
-    }
-
-    private void makeTail(DLLNode node) {
-
-        tail.next = node;
-        node.prev = tail;
-        tail = node;
-
     }
 
     public static void main(String[] args) {
