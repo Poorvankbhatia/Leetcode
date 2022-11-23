5f44da4a623032ddca389eb23982be102f3260ec
Poorvank Bhatia <poorvank@uber.com>
2022-11-23 23:24:29 +0530

    Refactor

--- src/graph/medium/CheapestFlight.java
+++ src/graph/medium/CheapestFlight.java
@@ -143,4 +143,28 @@ Dijikstra:
         return -1;
     }
 
+
+dp[i][j] means within i flights, the min cost to city j.
+
+
+
+    class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        double[][] dp = new double[K + 2][n];
+        for (int i = 0; i < K + 2; i++) {
+        	for (int j = 0; j < n; j++) {
+        		dp[i][j] = 1e9;
+        	}
+        }
+        dp[0][src] = 0;
+        for (int i = 1; i <= K + 1; i++) {
+        	dp[i][src] = 0;
+        	for (int[] flight : flights) {
+        		dp[i][flight[1]] = Math.min(dp[i][flight[1]], dp[i - 1][flight[0]] + flight[2]);
+        	}
+        }
+        return dp[K + 1][dst] >= 1e9 ? -1 : (int)dp[K + 1][dst];
+    }
+}
+
  */
